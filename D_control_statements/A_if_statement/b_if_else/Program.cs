a7627bce2a9766ea6ae01db60966052f063dbe7f
budhathokidai <budhathokirabi07@gmail.com>
2026-06-08 19:28:44 +0545

    THIRD COMMIT

---
+++ D_control_statements/A_if_statement/b_if_else/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace b_if_else;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a number between 1 to 100:");
+        int num=Convert.ToInt32(Console.ReadLine());
+        if(num % 2 == 0)
+        {
+            Console.WriteLine("The enetered number is prime!");
+        }
+        else
+        {
+            Console.WriteLine("The enterede number is composite!");
+        }
+    }
+}
+/*Enter a number between 1 to 100:
+2
+The enetered number is prime!*/
