eb8817cfec35609ab50c8501a7e1d204ae12ee96
budhathokidai <budhathokirabi07@gmail.com>
2026-06-11 19:55:49 +0545

    FOURTH COMMIT

---
+++ D_control_statements/B_loop_statement/a_for_loop/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace a_for_loop;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for(int i = 1; i <= 5; i++)
+        {
+            Console.WriteLine(i);
+        }
+        
+    }
+}
