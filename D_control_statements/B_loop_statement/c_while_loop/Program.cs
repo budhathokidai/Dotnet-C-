ab0be5a2c553f57f91491ef2b35cdcab02741eda
budhathokidai <budhathokirabi07@gmail.com>
2026-06-12 21:14:53 +0545

    FIFTH COMMIT

---
+++ D_control_statements/B_loop_statement/c_while_loop/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace c_while_loop;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int i=1;
+        while (i <= 5)
+        {
+            Console.WriteLine(i);
+            i++;
+        }
+        
+    }
+}
+/*
+1
+2
+3
+4
+5*/
