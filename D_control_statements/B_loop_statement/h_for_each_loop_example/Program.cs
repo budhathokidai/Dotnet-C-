ed8536abae0c64e83af91261e3017816588d04b3
budhathokidai <budhathokirabi07@gmail.com>
2026-06-14 14:57:57 +0545

    SIXTH COMMIT

---
+++ D_control_statements/B_loop_statement/h_for_each_loop_example/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace h_for_each_loop_example;
+//to print students  name 
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] names={"Rabi","Rewon","Kushal","Pratik","Sushil"};
+        foreach(string nam in names)
+        {
+            Console.WriteLine(nam);
+        }
+    }
+}
+
+
+/*Rabi
+Rewon
+Kushal
+Pratik
+Sushil*/
