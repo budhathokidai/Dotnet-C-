aba2129c9f75b0c7ac785cdd92f954176a1f62e7
budhathokidai <budhathokirabi07@gmail.com>
2026-06-17 19:18:02 +0545

    Switch Example

---
+++ D_control_statements/C_switch_statement/d_switch_month/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace d_switch_month;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a number from  1 to 12:");
+        int a = Convert.ToInt32(Console.ReadLine());
+        switch (a)
+        {
+            case 1:
+                Console.WriteLine("January ho!");
+                break;
+            case 2:
+                Console.WriteLine("Feburary ho!");
+                break;
+            case 3:
+                Console.WriteLine("March ho!");
+                break;
+            case 4:
+                Console.WriteLine("April ho!");
+                break;
+            case 5:
+            Console.WriteLine("May ho!");
+            break;
+            default:
+            Console.WriteLine("English month halana babu !");
+            break;
+        }
+    }
+}
+
+
+/*Enter a number from  1 to 12:
+3
+March ho!*/
---
+++ D_control_statements/C_switch_statement/e_vowel_consonent/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace e_vowel_consonent;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a letter:");
+        string? letter=Console.ReadLine();
+
+        switch (letter)
+        {
+            case "a":
+            Console.WriteLine("Its vowel!");
+            break;
+            case "e":
+            case "i":
+            case "o":
+            case "u":
+            Console.WriteLine("Its vowel!");
+            break;
+            default:
+            Console.WriteLine("its consonent!");
+            break;
+        }
+
+    
+    }
+}
+
+/*Enter a letter:
+u
+Its vowel!*/
