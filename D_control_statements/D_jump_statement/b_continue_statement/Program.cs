578f5fbfb2cf23033a9a6451ab6f1ed06ca057e6
budhathokidai <budhathokirabi07@gmail.com>
2026-06-18 20:34:23 +0545

    Continue Statement

---
+++ D_control_statements/D_jump_statement/b_continue_statement/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace b_continue_statement;
+//The continue statement skips the current iteration of a loop and continues with the next iteration.
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Continue statement");
+        for(int i = 1; i <= 5; i++)
+        {
+            if (i == 3)
+            {
+                continue;
+            }
+            Console.WriteLine(i);
+        }
+    }
+}
+
+/*
+Continue statement
+1
+2
+4
+5*/
