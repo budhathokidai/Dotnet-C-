bb2b3bc74f22892c8c208d288d0e21d3275d4fe4
budhathokidai <budhathokirabi07@gmail.com>
2026-06-18 20:33:37 +0545

    Break Statement

---
+++ D_control_statements/D_jump_statement/a_break_statement/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace a_break_statement;
+//The break statement is used to immediately exit a loop or a switch statement.
+class Program
+{
+    static void Main(string[] args)
+    {
+        for(int i = 1; i <= 5; i++)
+        {
+            if (i == 3)
+            {
+                break;
+            }
+            Console.WriteLine(i);
+        }
+    }
+}
+/*
+1
+2*/
