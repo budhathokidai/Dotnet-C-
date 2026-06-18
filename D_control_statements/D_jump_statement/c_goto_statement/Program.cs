3560c2ade3fb72951870f690caf0dd94410f6eeb
budhathokidai <budhathokirabi07@gmail.com>
2026-06-18 20:34:48 +0545

    Goto Statement

---
+++ D_control_statements/D_jump_statement/c_goto_statement/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace c_goto_statement;
+//The goto statement transfers control directly to a labeled statement within the same method.
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Goto Statement");
+
+        int i = 1;
+
+    Start:
+        if (i <= 5)
+        {
+            Console.WriteLine(i);
+            i++;
+            goto Start;
+        }
+
+        Console.WriteLine("Loop Finished");
+    }
+}
+
+/*
+Goto Statement
+1
+2
+3
+4
+5
+Loop Finished*/
