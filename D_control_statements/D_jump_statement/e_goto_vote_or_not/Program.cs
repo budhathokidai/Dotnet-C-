21ff1b554ba162ba784346719191eb9255954349
budhathokidai <budhathokirabi07@gmail.com>
2026-06-19 09:35:46 +0545

    Goto example

---
+++ D_control_statements/D_jump_statement/e_goto_vote_or_not/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace e_goto_vote_or_not;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+    ineligible:
+       
+
+        Console.WriteLine("Enter your age:");
+        int age = Convert.ToInt32(Console.ReadLine());
+
+        if (age < 18)
+        {
+             Console.WriteLine("You cannot  vote.");
+            goto ineligible;
+        }
+        else
+        {
+            Console.WriteLine("You can vote.");
+        }
+    }
+}
+
+/*Enter your age:
+5
+You cannot  vote.
+Enter your age:
+67
+You can vote.*/
