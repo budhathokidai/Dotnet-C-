37586f91bf0b7c5197e82dc531710599a11a5320
budhathokidai <budhathokirabi07@gmail.com>
2026-06-19 09:36:19 +0545

    Goto second example

---
+++ D_control_statements/D_jump_statement/f_pass_fail/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace f_pass_fail;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+    retry:
+        Console.Write("Enter your marks: ");
+        int marks = Convert.ToInt32(Console.ReadLine());
+
+        if (marks < 40)
+        {
+            Console.WriteLine("You are Fail.");
+            goto retry;
+        }
+
+        Console.WriteLine("Congratulations! You are Pass.");
+    }
+}
+
+/*Enter your marks: 34
+You are Fail.
+Enter your marks: 67
+Congratulations! You are Pass.*/
