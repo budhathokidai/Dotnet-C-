a8b6c1b11535e6095f8631afa3128220942d0aeb
budhathokidai <budhathokirabi07@gmail.com>
2026-07-04 11:58:16 +0545

    Classwork_Array

---
+++ F_array/e_sort_array/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace e_sort_array;
+
+class Program
+{
+    static void Main(string[] args)
+     { 
+        int[] myarray={100,8,7,45,68,67};
+        Console.WriteLine("Array before sorting");
+        foreach(var item in myarray)
+        {
+            Console.WriteLine(item);
+        }
+        Array.Sort(myarray);
+        Console.WriteLine("Array after sorting");
+        foreach(var item in myarray)
+        {
+            Console.WriteLine(item);
+        }
+      
+    }
+}
+
