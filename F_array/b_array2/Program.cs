ab329ddfe921f77e55efedd844ef0c4b8cf0abe9
budhathokidai <budhathokirabi07@gmail.com>
2026-07-04 11:57:04 +0545

    Classwork_Array

---
+++ F_array/b_array2/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace b_array2;
+
+class Program
+{
+    static void Main(string[] args)
+    { 
+        int[] myarray={1,2,3,4,54};
+        foreach(var item in myarray)
+        {
+            Console.WriteLine("{0}\t",item);
+        }
+      
+    }
+}
