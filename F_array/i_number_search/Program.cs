bf08ad2faee2ef209444ef76837e65441e8064b0
budhathokidai <budhathokirabi07@gmail.com>
2026-07-04 11:59:13 +0545

    Classwork_Array

---
+++ F_array/i_number_search/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace i_number_search;
+#nullable disable
+class Program
+{
+    static void Main(string[] args)
+    {
+        int [] myarray={1,43,5,4,76,86,200};
+        Console.WriteLine("Enter a number to search it in array");
+        int num=int.Parse(Console.ReadLine());
+        bool found =false;
+        for(int i = 0; i < myarray.Length; i++)
+        {
+            if(num == myarray[i])
+            {
+                found=true;
+                break;
+            }
+        }
+        if(found == true)
+        {
+            Console.WriteLine("Number Found");
+        }
+        else
+        {
+            Console.WriteLine("Number  not found");
+        }
+
+    }
+}
