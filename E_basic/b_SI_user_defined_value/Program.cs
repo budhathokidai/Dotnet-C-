edb22a7ccad31213d1a0992f385b1b589edf79ce
budhathokidai <budhathokirabi07@gmail.com>
2026-07-04 11:55:09 +0545

    Classowrk

---
+++ E_basic/b_SI_user_defined_value/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace b_SI_user_defined_value;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int P=50000;
+        double R=4.5;
+        int t=5;
+        double SI=(P*t*R)/100;
+
+        Console.WriteLine("Simple intrest ="+SI);
+    }
+}
