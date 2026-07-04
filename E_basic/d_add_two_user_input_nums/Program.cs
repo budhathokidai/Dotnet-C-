3589faf0e16f42db603393904a48924dec33a378
budhathokidai <budhathokirabi07@gmail.com>
2026-07-04 11:55:43 +0545

    Classowrk

---
+++ E_basic/d_add_two_user_input_nums/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace d_add_two_user_input_nums;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter first number:");
+        int a=Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter second number:");
+        int b=Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("The sum of two number:"+(a+b));
+    }
+}
