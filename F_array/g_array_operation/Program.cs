d662710863d99f2f1696ba18acaeb551fee047c0
budhathokidai <budhathokirabi07@gmail.com>
2026-07-04 11:58:45 +0545

    Classwork_Array

---
+++ F_array/g_array_operation/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace g_array_operation;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int [] myarray={1,45,5,6,4,3,2};
+        Console.WriteLine("Minimum value:"+myarray.Min());
+        Console.WriteLine("Maximum value:"+myarray.Max());
+        Console.WriteLine("Sum value:"+myarray.Sum());
+        Console.WriteLine("Average value:"+myarray.Average());
+
+    }
+}
