d456125ad783d6a51cefea20156c751218e331c4
budhathokidai <budhathokirabi07@gmail.com>
2026-07-04 11:57:17 +0545

    Classwork_Array

---
+++ F_array/c_char_array/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace c_char_array;
+
+class Program
+{
+    static void Main(string[] args)
+     { 
+        char[] myarray={'r','a','b','i'};
+        foreach(var item in myarray)
+        {
+            Console.WriteLine("{0}\t",item);
+        }
+      
+    }
+}
