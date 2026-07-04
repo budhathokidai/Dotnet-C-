e56498215f936331aead331cd21d2a6483f9fba7
budhathokidai <budhathokirabi07@gmail.com>
2026-07-04 11:58:59 +0545

    Classwork_Array

---
+++ F_array/h_string_sort/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace h_string_sort;
+
+class Program
+{
+    static void Main(string[] args)
+     { 
+        string[] myarray={"ram","Hari","Sita","Gita"};
+        Console.WriteLine("Array before sorting");
+        foreach(var item in myarray)
+        {
+            Console.WriteLine(item);
+        }
+        Array.Sort(myarray);
+        Console.WriteLine("Array after sorting");
+        foreach(var item in myarray)
+        {
+            Console.WriteLine(item);
+        }
+      
+    }
+}
