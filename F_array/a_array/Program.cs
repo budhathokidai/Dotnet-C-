53f2c5dafe06a37656cc4d5fdc7bb84d9a86bfd7
budhathokidai <budhathokirabi07@gmail.com>
2026-07-04 11:56:43 +0545

    Classwork_Array

---
+++ F_array/a_array/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace a_array;
+
+class Program
+{
+    static void Main(string[] args)
+    
+    {
+        int[] myarray={1,2,3,4,54};
+        for(int i=0;i<5;i++)
+        {
+            Console.WriteLine(myarray[i]);  
+        }
+      
+    }
+}
