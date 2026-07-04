7d73c85c6a6f7a71db8368af79f837563f2c15fa
budhathokidai <budhathokirabi07@gmail.com>
2026-07-04 11:58:32 +0545

    Classwork_Array

---
+++ F_array/f_array_reverse/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace f_array_reverse;
+
+class Program
+{
+    
+    static void Main(string[] args)
+     { 
+        int[] myarray={100,8,7,45,68,67};
+        Console.WriteLine("Array before sorting");
+        foreach(var item in myarray)
+        {
+            Console.WriteLine(item);
+        }
+        Array.Sort(myarray);
+        Array.Reverse(myarray);
+        Console.WriteLine("Array after sorting");
+        foreach(var item in myarray)
+        {
+            Console.WriteLine(item);
+        }
+      
+    }
+}
