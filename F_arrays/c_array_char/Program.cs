a024d9e801f432805ba4dc308e49152c968fe4da
budhathokidai <budhathokirabi07@gmail.com>
2026-07-11 14:41:12 +0545

    Aarray len

---
+++ F_arrays/c_array_char/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace c_array_char;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        char[] character={'R','A','B','I'};
+        foreach(var num in character)
+        {
+            Console.Write("\t{0}",num);
+        }
+    }
+}
+/*    R       A       B       I*/
