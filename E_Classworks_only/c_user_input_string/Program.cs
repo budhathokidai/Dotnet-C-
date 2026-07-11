732b4f24a58f5320bb80aca980678185e3135f3c
budhathokidai <budhathokirabi07@gmail.com>
2026-07-11 14:11:20 +0545

    string user input

---
+++ E_Classworks_only/c_user_input_string/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace c_user_input_string;
+#nullable disable  //insted of ? according to requirement
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter your name:");
+        string name=Console.ReadLine();
+        Console.WriteLine("Your name is :"+name);
+    }
+}
