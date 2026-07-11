71dbc324c671298339ebb75d2cc276b67bba6ab2
budhathokidai <budhathokirabi07@gmail.com>
2026-07-11 14:10:42 +0545

    SI

---
+++ E_Classworks_only/b_simple_interest/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace b_simple_interest;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double p = 12000;
+        double t = 3;
+        double r = 5;
+        double si = (p*t*r)/100;
+        Console.WriteLine("Simple Interest :" + si);
+
+    }
+}
