4c4a34f770b37bd610f990a1a1e58d9e362f4914
budhathokidai <budhathokirabi07@gmail.com>
2026-07-11 14:40:43 +0545

    Array length

---
+++ F_arrays/b_array_length/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace b_array_length;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] arrays={3,4,5,6,7};
+        for(int i=0; i< arrays.Length; i++)
+        {
+            Console.WriteLine(arrays[i]);
+        } 
+        
+    }
+}
+/*
+3
+4
+5
+6
+7*/
