c2a254a1c3473b0d3e9b6438abb5fe46d97d9fc3
budhathokidai <budhathokirabi07@gmail.com>
2026-07-11 17:42:13 +0545

    String sort

---
+++ F_arrays/h_array_String_sort/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace h_array_String_sort;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] strings = { "name", "rollno", "address" };
+        /*foreach(var str in strings)
+        {
+            Console.WriteLine(str);
+        }
+        Array.Sort(strings);
+        Console.WriteLine("Strings after sorting");
+        foreach(var str in strings)
+        {
+            Console.WriteLine(str);
+        }*/
+
+        int len = strings.Length;
+
+        Console.WriteLine("The number of array element is = " + len);
+        Console.WriteLine("\nList of array elements before sorting:");
+        for (int i = 0; i < len; i++)
+        {
+            Console.WriteLine("Name {0} is = {1} ", i + 1, strings[i]);
+        }
+        // Array.Sort(strings); // sorts in ascending order
+        Array.Reverse(strings); // sorts in descending order
+        Console.WriteLine("\nList of array elements after sorting:");
+        for (int i = 0; i < len; i++)
+        {
+            Console.WriteLine("Name {0} is = {1} ", i + 1, strings[i]);
+        }
+    }
+}
+/*List of array elements after sorting:
+Name 1 is = address 
+Name 2 is = rollno 
+Name 3 is = name */
