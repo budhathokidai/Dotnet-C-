d66dadecba8f7bd8f32e8b6704ee8f9fac19cf9d
budhathokidai <budhathokirabi07@gmail.com>
2026-07-11 14:12:07 +0545

    user_input_add_nums

---
+++ E_Classworks_only/d_user_input_add_nums/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace d_user_input_add_nums;
+#nullable disable
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter first number:");
+        int num1=int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter second number:");
+        int num2=Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("The sum is:"+(num1+num2));
+
+    }
+}
+/*
+Enter first number:
+23
+Enter second number:
+4
+The sum is:27*/
