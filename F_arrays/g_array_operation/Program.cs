96e3a124da76b8d7ed863ae341d187f54ee888e4
budhathokidai <budhathokirabi07@gmail.com>
2026-07-11 17:40:46 +0545

    Array operation

---
+++ F_arrays/g_array_operation/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace g_array_operation;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers={1,23,4,4,5,6,34,56,4564,456,456};
+        Console.WriteLine("Maximum value form numbers:"+numbers.Max());
+        Console.WriteLine("Minimum value form numbers:"+numbers.Min());
+        Console.WriteLine("Sum of all array elements:"+numbers.Sum());
+        Console.WriteLine("Average of all array elements:"+numbers.Average());
+    }
+}
+
+/*
+Maximum value form numbers:4564
+Minimum value form numbers:1
+Sum of all array elements:5609
+Average of all array elements:509.90909090909093*/
