452d341cee16d132182af21e7a50e24bad9cb4b1
budhathokidai <budhathokirabi07@gmail.com>
2026-07-11 14:41:47 +0545

    Array len2

---
+++ F_arrays/d_array_char/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace d_array_char;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] strings={"Ram","Hari","Hami"};
+        foreach(var str in strings)
+        {
+             Console.Write("\t{0}",str);
+        }
+       
+    }
+}
+/*        Ram     Hari    Hami*/
