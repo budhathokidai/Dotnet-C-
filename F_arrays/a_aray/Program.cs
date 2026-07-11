0e7bda1c725bcd3dc2d5ac001b323dc65c50c548
budhathokidai <budhathokirabi07@gmail.com>
2026-07-11 14:39:52 +0545

    Array1

---
+++ F_arrays/a_aray/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace a_aray;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int [] array={12,3,4,45,5,6};
+        foreach(var item in array)
+        {
+            Console.WriteLine(item);
+            //Console.WriteLine("{0\t}",item);
+        }
+        
+    }
+}
