5f455dd0445e4fb47f4400d8a2935e29af9c4b12
budhathokidai <budhathokirabi07@gmail.com>
2026-07-11 14:13:23 +0545

    enumerations

---
+++ E_Classworks_only/f_enum_hello/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace f_enum_hello;
+
+class Program
+{ 
+    enum Days{Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Sturday};
+    static void Main(string[] args)
+    { 
+        var wd=Days.Tuesday;
+        if(wd==Days.Sunday || wd==Days.Monday)
+        {
+            Console.WriteLine("Hello ist not the day you wnat!");
+        }
+        else
+        {
+            Console.WriteLine("Hello ram its the day "+wd);
+        }
+    }
+}
