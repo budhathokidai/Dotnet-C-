75a2e7a35d73348e1ca2cd6753d189379fc9e14f
budhathokidai <budhathokirabi07@gmail.com>
2026-07-12 19:44:51 +0545

    Compatible

---
+++ G_type_conversion/c_non_compatibal/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace c_non_compatibal;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string  a="123";
+        int b=Convert.ToInt32(a);          //int b=(int)a (its not possible)
+        //int b=int.Parse(a);  Parse method
+        int c=354;
+        int sum=b+c;
+        Console.WriteLine("Sum="+sum);
+    }
+}
+
+/* Convert Classes
+ToByte()
+ToInt16() = > short
+ToInt32() = > int
+ToInt64() = > long */
