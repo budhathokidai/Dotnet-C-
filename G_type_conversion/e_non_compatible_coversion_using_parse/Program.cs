e45a4c05fe4c087311a5aafe9faa3c76e7f674ba
budhathokidai <budhathokirabi07@gmail.com>
2026-07-12 19:46:03 +0545

    Non_compatible_using_prase

---
+++ G_type_conversion/e_non_compatible_coversion_using_parse/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace e_non_compatible_coversion_using_parse;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string a="234";
+        int b=int.Parse(a);
+        Console.WriteLine(a);
+        Console.WriteLine(b);
+    }
+}
+/*234
+234*/
+
+/* Every data type has a parse method
+Byte.Parse();
+short.Parse();
+int.Parse();
+bool.Parse(); */
