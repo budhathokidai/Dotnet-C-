e3bd866ca34af2639dee1231d3161ab159b9d7b1
budhathokidai <budhathokirabi07@gmail.com>
2026-07-12 19:45:19 +0545

    Explicit

---
+++ G_type_conversion/d_explicit/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace d_explicit;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*double a = 13.46;
+        int b = (int)a;
+        Console.WriteLine("Double value = " + a);
+        Console.WriteLine("Converted integer value = " + b);*/
+
+        int a=234;
+        byte b=(byte)a;  //byte range 0-255
+        Console.WriteLine("Integer  value="+a);
+        Console.WriteLine("Byte value="+b);
+    }
+}
+/*Double value = 13.46
+Converted integer value = 13*/
+
+/*Integer  value=234
+Byte value=234*/
