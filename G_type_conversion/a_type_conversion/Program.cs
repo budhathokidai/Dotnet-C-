7634c672bfd5ca7c9dd400d09306d9e83f7fb9db
budhathokidai <budhathokirabi07@gmail.com>
2026-07-12 19:43:58 +0545

    Implicit Baisc

---
+++ G_type_conversion/a_type_conversion/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace a_type_conversion;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        byte a=10;
+        //float b=a;//implicite
+        float b=(float)a; //explisit i.e Type Casting
+        Console.WriteLine("Byte value="+a);
+        Console.WriteLine("Float value="+b);
+
+    }
+}
