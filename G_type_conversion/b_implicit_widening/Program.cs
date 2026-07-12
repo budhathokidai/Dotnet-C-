bfaecfeb83ca0ccc915578227c485e71ececb2aa
budhathokidai <budhathokirabi07@gmail.com>
2026-07-12 19:44:26 +0545

    Implicit

---
+++ G_type_conversion/b_implicit_widening/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace b_implicit_widening;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //byte a=256;// data type in byte ranges from 0 -255
+        byte a=254;
+        int b=a;
+        Console.WriteLine("Byte value="+a);
+        Console.WriteLine("Integer value="+b);
+
+        /*byte a = 10;
+        int b = a;
+        long c = a;
+
+        /* int b = (int)a;
+        long c = (long)a; */
+
+        /*Console.WriteLine("Byte a =" + a);
+        Console.WriteLine("Integer b = " + b);
+        Console.WriteLine("Long integer c = " + c);*/
+
+
+
+       
+    }
+}
+/*Byte value=254
+Integer value=254*/
