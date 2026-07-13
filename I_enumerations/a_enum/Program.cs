8700ad22a11a664d5552d5dad9c8b843f98260e3
budhathokidai <budhathokirabi07@gmail.com>
2026-07-13 21:07:26 +0545

    E_num basic

---
+++ I_enumerations/a_enum/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace a_enum;
+//An enum (enumeration) is a user-defined data type that is used to store a fixed set of named constants.
+class Program
+{
+    enum Days{Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Sturday};
+    static void Main(string[] args)
+    {
+        int firstDay=(int)Days.Sunday;
+        Console.WriteLine(firstDay);
+
+        var wd=(Days)5;
+        Console.WriteLine(wd);
+    }
+}
+/*0
+Friday*/
