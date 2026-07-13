2803c8dbe18aa4a878c6484c35cf75cb8cbcc852
budhathokidai <budhathokirabi07@gmail.com>
2026-07-13 21:04:28 +0545

    Area of rectangle

---
+++ H_structure/e_area_of_rectangle/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Drawing;
+
+namespace e_area_of_rectangle;
+
+struct Rectangle
+{
+    public int length;
+    public int breadth;
+
+    static void Main(string[] args)
+    {
+        Rectangle r;
+        r.length=3;
+        r.breadth=4;
+        int A=r.length*r.breadth;
+        Console.WriteLine("Area of rectangle="+A);
+    }
+}
+/*Area of rectangle=12*/
