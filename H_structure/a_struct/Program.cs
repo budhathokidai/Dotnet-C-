2acb2fca9831b1ef9b097291c94313fd9183fa3f
budhathokidai <budhathokirabi07@gmail.com>
2026-07-13 20:28:55 +0545

    Structure Basic

---
+++ H_structure/a_struct/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace a_struct;
+
+struct Student
+{
+    public int roll;
+    public void display(int roll)
+    {
+        Console.WriteLine("Roll number="+roll);
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Student s1;
+        s1.roll=45;
+        Console.WriteLine(s1.roll);
+    }
+}
+
+/*45*/
