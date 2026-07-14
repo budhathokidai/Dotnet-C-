f855c10a3763f513e128a507b0db2c0f08dae4b6
budhathokidai <budhathokirabi07@gmail.com>
2026-07-14 10:33:34 +0545

    Enums_display

---
+++ I_enumerations/c_enums_display/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace c_enums_display;
+#nullable disable
+class Program
+{
+    enum Days{Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Sturday};  //anywhere we can 
+    static void Main(string[] args)
+    {
+        string name ;
+        Days day;
+        Console.WriteLine("Enter your name: ");
+        name=Console.ReadLine();
+        Console.WriteLine("Enter your values from (0-6)for days");
+        int index=int.Parse(Console.ReadLine());
+        day=(Days)index; ///typecasting
+        Console.WriteLine("Hello "+name+ "today is "+day+".");
+    }
+}
