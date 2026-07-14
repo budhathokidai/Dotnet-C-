c163d3b70eb5eb07e4a9a60d92e3eda4ea57b166
budhathokidai <budhathokirabi07@gmail.com>
2026-07-14 10:34:48 +0545

    Student_grade

---
+++ I_enumerations/e_student_grade/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace e_student_grade;
+
+class Program
+{
+    enum Grades{A,B,C,D,E};
+    static void Main(string[] args)
+    {
+        Grades stdgrade=Grades.A;
+         Console.WriteLine("Grade: " + stdgrade);
+    }
+}
