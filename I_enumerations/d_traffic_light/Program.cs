adf336748fbc7387a4a22580a2f1f60df495fd02
budhathokidai <budhathokirabi07@gmail.com>
2026-07-14 10:34:11 +0545

    Traffic_Light

---
+++ I_enumerations/d_traffic_light/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace d_traffic_light;
+
+class Program
+{
+      enum Lights{Red,Yellow,Green};
+    static void Main(string[] args)
+    {
+        var Traffic=Lights.Yellow;
+        if (Traffic == Lights.Red)
+        {
+            Console.WriteLine("Stop where you are!");
+        }
+        else if (Traffic == Lights.Green)
+        {
+            Console.WriteLine("Goooooooo!");
+        }
+        else
+        {
+            Console.WriteLine("Daya Baya herau!");
+        }
+    }
+}
