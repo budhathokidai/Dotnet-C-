12e3f71d116291f027b9bcffeba5c1ef8acc7606
budhathokidai <budhathokirabi07@gmail.com>
2026-07-15 09:51:01 +0545

    Parameterized

---
+++ N_Constructor_Destructor/b_parameterized_constructor/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace b_parameterized_constructor;
+class Employee
+{
+    public int age;
+    public string name;
+    public string address;
+
+    public  Employee(int a,string n,string ad)
+    {
+        age=a;
+        name=n;
+        address=ad;
+    }
+    public void displaydata()
+    {
+        Console.WriteLine("\nEmployee Details");
+         Console.WriteLine("----------------");
+        Console.WriteLine("Age="+age);
+        Console.WriteLine("Name="+name);
+        Console.WriteLine("Address="+address);
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Employee e=new Employee(10,"Hari","Ktm");
+        e.displaydata();
+       
+    }
+}
