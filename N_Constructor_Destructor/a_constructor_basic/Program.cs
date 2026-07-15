7a6d55fa77ef16f4be3552e9b9ae4ff123877025
budhathokidai <budhathokirabi07@gmail.com>
2026-07-15 09:50:42 +0545

    Constructor

---
+++ N_Constructor_Destructor/a_constructor_basic/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace a_constructor_basic;
+class Employee
+{
+    public Employee()
+    {
+        Console.WriteLine("Hello employee!");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Employee e=new Employee();
+        //new Employee():
+    }
+}
+/*Hello employee!*/
