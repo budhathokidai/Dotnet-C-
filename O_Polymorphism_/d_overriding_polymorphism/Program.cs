abdc70c126c4f5c194be9faff55ca349aa04f296
budhathokidai <budhathokirabi07@gmail.com>
2026-07-16 08:47:16 +0545

    overiding

---
+++ O_Polymorphism_/d_overriding_polymorphism/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace d_overriding_polymorphism;
+
+class Ayodhya
+{
+    public void jay()
+    {
+        Console.WriteLine("Hello Ram!");
+    }
+}
+class Ram : Ayodhya
+{
+    public new void jay()  //importnat
+    {
+        Console.WriteLine("Jay shree ram...!");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Ram r1=new Ram();
+        r1.jay();
+    }
+}
+
+/*Jay shree ram...!*/
