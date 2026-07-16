ed38fda3282fef665d3b5919f7a0be4dc15f6051
budhathokidai <budhathokirabi07@gmail.com>
2026-07-16 08:46:23 +0545

    overloading_diff_datatypes

---
+++ O_Polymorphism_/b_overloading_different_data_types/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace b_overloading_different_data_types;
+
+class Numbers
+{
+    public int subtract(int a,int b,int c)
+    {
+        return(a+b+c);
+    }
+    public double subtract(double a,double b)
+    {
+        return(a+b);
+    }
+
+  public string subtract(string a,string b)
+    {
+        return(a+b);
+    }
+
+    
+
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Numbers n1=new Numbers();
+        Numbers n2=new Numbers();
+        Numbers n3=new Numbers();
+        Console.WriteLine("The sum of numbers = "+n1.subtract(1,2,3));
+        Console.WriteLine("The sum ="+n2.subtract(1.3,2.5));
+        Console.WriteLine("The sum ="+n3.subtract("Hello","Rabi"));
+    }
+}
+
+
+/*The sum of numbers = 6
+The sum =3.8
+The sum =HelloRabi*/
