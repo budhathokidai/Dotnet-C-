07be5c4741712bc96313577a61032e2ece9321c1
budhathokidai <budhathokirabi07@gmail.com>
2026-07-16 08:46:48 +0545

    Different_sequence

---
+++ O_Polymorphism_/c_Different_sequence _of_parameter/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace c_Different_sequence__of_parameter;
+class Myclass
+{
+    public void displayDetails(int roll,string name)
+    {
+        Console.WriteLine("Roll ={0} and name = {1}",roll,name);
+    }
+    public void displayDetails(string name,int roll)
+    {
+        Console.WriteLine("Roll ={0} and name = {1}",roll,name);
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Myclass m1=new Myclass();
+        m1.displayDetails(10,"Rabi");
+        m1.displayDetails("Hari",20);
+    }
+}
+
+/*Roll =10 and name = Rabi
+Roll =20 and name = Hari*/
