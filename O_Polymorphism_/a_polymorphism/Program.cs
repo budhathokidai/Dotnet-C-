8966d6954cb0965b1eb3485ea9ef2ec4a0d06ea4
budhathokidai <budhathokirabi07@gmail.com>
2026-07-16 08:10:22 +0545

    polymorphism

---
+++ O_Polymorphism_/a_polymorphism/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace a_polymorphism;
+class Numbers
+{
+    public int add(int a,int b,int c)
+    {
+        return(a+b+c);
+    }
+    public int add(int a,int b)
+    {
+        return(a+b);
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Numbers n1=new Numbers();
+        Numbers n2=new Numbers();
+        Console.WriteLine("The sum of numbers = "+n1.add(1,2,3));
+        Console.WriteLine("The sum ="+n2.add(1,2));
+    }
+}
+
+/*The sum of numbers = 6
+The sum =3*/
