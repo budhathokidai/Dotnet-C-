ba0f352438e7b2135f28c6bc54851188fe5bc880
budhathokidai <budhathokirabi07@gmail.com>
2026-07-16 08:49:42 +0545

    interface

---
+++ P_interface/a_interface/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace a_interface;
+interface IGadgets
+{
+    public void brand();
+}
+class Laptop : IGadgets
+{
+    public void brand()
+    {
+        Console.WriteLine("laptop is of dell brand! ");
+    }
+}
+
+class Mobile : IGadgets
+{
+    public void brand()
+    {
+        Console.WriteLine("mobile is of nothing brand! ");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*Laptop l1=new Laptop();
+        l1.brand();
+        Mobile m1=new Mobile();
+        m1.brand();*/
+        IGadgets hp1=new Laptop();
+        IGadgets a4=new Mobile();
+        hp1.brand();
+        a4.brand();
+        
+    }
+}
+
+/*laptop is of dell brand! 
+mobile is of nothing brand! */
