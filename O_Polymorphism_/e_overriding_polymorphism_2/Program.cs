208e396b00161a9a0924de4f0f20609073e514ac
budhathokidai <budhathokirabi07@gmail.com>
2026-07-16 08:47:51 +0545

    overring_with_virtual

---
+++ O_Polymorphism_/e_overriding_polymorphism_2/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace e_overriding_polymorphism_2;
+class Ayodhya
+{
+    public virtual void jay()
+    {
+        Console.WriteLine("Hello Ram!");
+    }
+}
+class Ram : Ayodhya
+{
+    public override void jay()  //importnat
+    {
+        Console.WriteLine("Jay shree ram...!");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Ram r1=new Ram();
+        Ayodhya r1=new Ram();
+        r1.jay();
+    }
+}
+
+/*Jay shree ram...!*/
