a2016e6099644df4aa145ebec971d77f001d9577
budhathokidai <budhathokirabi07@gmail.com>
2026-07-16 08:51:02 +0545

    problem_fix

---
+++ P_interface/c_multiple_interface_problem_fix/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace c_multiple_interface_problem_fix;
+
+interface IFather
+{
+    void FatherMethod();
+}
+interface IMother
+{
+    void MotherMethod();
+}
+
+class Child : IFather, IMother
+{
+    public void FatherMethod()
+    {
+        Console.WriteLine("Hello i am dad");
+    }
+    public void MotherMethod()
+    {
+        Console.WriteLine("Hello i am mother");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Child c1 = new Child();
+        c1.FatherMethod();
+        c1.MotherMethod();
+    }
+}
+
+/*Hello i am dad
+Hello i am mother*/
