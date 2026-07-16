4104c7b9e5780caa92dc61d2580f6d063a8c5eb4
budhathokidai <budhathokirabi07@gmail.com>
2026-07-16 08:51:39 +0545

    interface_inheriting_interface

---
+++ P_interface/d_interface_inherit_interface/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace d_interface_inherit_interface;
+
+interface ISchool
+{
+    void Admin();
+}
+
+interface ISir : ISchool
+{
+    void Teach();
+}
+
+class Mam : ISir
+{
+    public void Admin()
+    {
+        Console.WriteLine("I am admin of all");
+    }
+
+    public void Teach()
+    {
+        Console.WriteLine("I teach under admin instructions!");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       Mam d = new Mam();
+
+        d.Admin();
+        d.Teach();
+    }
+}
+
+/*I am admin of all
+I teach under admin instructions!*/
