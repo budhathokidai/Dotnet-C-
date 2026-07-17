4b83478ec83940472c4a5dbfd8f24095c9ef1621
budhathokidai <budhathokirabi07@gmail.com>
2026-07-17 20:20:20 +0545

    a_main

---
+++ J_partial_class/a_partial_class/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace a_partial_class;
+ //partial class is a class whose code is split into two or more files. All the parts are combined by the compiler to make one single class.
+ class Program
+{
+    static void Main(string[] args)
+    {
+       Student s1= new Student();
+       s1.hi();
+       s1.hello();
+    }
+}
