8b7901cb8e91420eadefc29d5ca333d496dc056d
budhathokidai <budhathokirabi07@gmail.com>
2026-07-17 20:23:41 +0545

    Main

---
+++ J_partial_class/c_partial_class_3/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace c_partial_class_3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Employee e =new Employee(101,"Mina");
+        e.Salary=3400;
+        e.Display();
+    }
+}
