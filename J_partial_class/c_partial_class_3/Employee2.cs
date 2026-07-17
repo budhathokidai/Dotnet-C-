991961a5f36705a8fda4cfada728b7376928acfc
budhathokidai <budhathokirabi07@gmail.com>
2026-07-17 20:24:35 +0545

    File_2

---
+++ J_partial_class/c_partial_class_3/Employee2.cs
@@ -0,0 +1,11 @@
+partial class Employee
+{
+     public double Salary;
+
+    public void Display()
+    {
+        Console.WriteLine("ID: " + Id);
+        Console.WriteLine("Name: " + Name);
+        Console.WriteLine("Salary: " + Salary);
+    }
+}
