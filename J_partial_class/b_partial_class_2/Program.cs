acfa9831a5cccc0b57765b03bba236b6bdee0f3e
budhathokidai <budhathokirabi07@gmail.com>
2026-07-17 20:21:54 +0545

    Main

---
+++ J_partial_class/b_partial_class_2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.Intrinsics.Arm;
+
+namespace b_partial_class_2;
+
+class Program
+{
+    static void Main(string[] args)
+    
+    {
+        Studnet s1=new Studnet();
+        string UserInput=s1.getInput();
+        s1.displaydata(UserInput);
+        
+    }
+}
