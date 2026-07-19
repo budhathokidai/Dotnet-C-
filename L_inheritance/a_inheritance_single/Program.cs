a7317dd67bf4d1fc3d83aee221e9ac8a85e7855d
budhathokidai <budhathokirabi07@gmail.com>
2026-07-19 13:53:07 +0545

    Inheritance

---
+++ L_inheritance/a_inheritance_single/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace a_inheritance_single;
+
+class Parents
+{
+    public void hello()
+    {
+        Console.WriteLine("I am parent of my child!");
+    }
+}
+class Child : Parents
+{
+    public void hi()
+    {
+        Console.WriteLine("I am child of my parent!");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Child c1 = new Child();
+        c1.hello();
+        c1.hi();
+    }
+}
