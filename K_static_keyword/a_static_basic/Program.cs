6d8f3b4ae7a2a41c328806c47251a07608ca7442
budhathokidai <budhathokirabi07@gmail.com>
2026-07-19 13:52:46 +0545

    Static_Keyword

---
+++ K_static_keyword/a_static_basic/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace a_static_basic;
+//The static keyword is used to make a variable, method, constructor, or class belong to the class itself, not to any object.
+static class Shapes
+{
+    public static int len=56;
+}
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("The length of Shape="+Shapes.len);
+    }
+}
