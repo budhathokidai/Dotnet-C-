50938d301e577bbd44fefad4ea6f42123023b035
budhathokidai <budhathokirabi07@gmail.com>
2026-07-19 13:53:45 +0545

    Sealed_Keyword

---
+++ M_sealed_keyword/a_sealed/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace a_sealed;
+// sealed class ko agadi,method ko agadi
+sealed class Dad   //a sealed class can have objects.
+{
+    public void hi()
+    {
+        Console.WriteLine("Hi ma chai father!");
+    }
+}
+class Son 
+{
+    public void hello()
+    {
+        Console.WriteLine("Hello i am son!");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Son s=new Son();//object of son!
+        //s.hi();
+        s.hello();
+    }
+}
