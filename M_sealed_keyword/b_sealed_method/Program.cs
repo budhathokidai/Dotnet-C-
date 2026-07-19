1358716c135ac0f3e0d1f0aa62282d10beb6f08f
budhathokidai <budhathokirabi07@gmail.com>
2026-07-19 14:15:30 +0545

    Sealed_Keyword

---
+++ M_sealed_keyword/b_sealed_method/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace b_sealed_method;
+class Utensils
+{
+    public virtual void Sound()  //only a virtual method can be overridden in a child class.
+    {
+        Console.WriteLine("Utensils makes a sound when we drop from high.");
+    }
+}
+
+class Plate :Utensils
+{
+    public sealed override void Sound()
+    {
+        Console.WriteLine("Plate makes sound.");
+    }
+}
+
+class Bowl: Plate
+{
+    //A sealed method cannot be overridden further
+}
+
+class Program
+{
+    static void Main()
+    {
+        Plate d = new Plate();
+        d.Sound();
+    }
+}
+
+//Plate makes sound.
