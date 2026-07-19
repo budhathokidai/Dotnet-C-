5687b70e460fc0842444c4066debd93f68f31535
budhathokidai <budhathokirabi07@gmail.com>
2026-07-19 13:50:38 +0545

    Enum

--- I_enumerations/b_enums_arrange/Program.cs
+++ I_enumerations/b_enums_arrange/Program.cs
@@ -2,18 +2,18 @@
 
 class Program
 {
-     enum Days{Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday};
-    static void Main(string[] args)
-    {
-        var today=Days.Sunday;
-        if(today==Days.Friday || today == Days.Saturday)
-        {
-             Console.WriteLine("Lets go for a round");
-        }
-        else
-        {
-             Console.WriteLine("This is work days ,go to work!");
-        }
-       
-    }
+     enum Days { Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday };
+     static void Main(string[] args)
+     {
+          var today = Days.Sunday;
+          if (today == Days.Friday || today == Days.Saturday)
+          {
+               Console.WriteLine("Lets go for a round");
+          }
+          else
+          {
+               Console.WriteLine("This is work days ,go to work!");
+          }
+
+     }
 }
