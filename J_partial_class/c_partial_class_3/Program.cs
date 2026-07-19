8a3a062d2736798d128ac8f4f86d93a64a31f134
budhathokidai <budhathokirabi07@gmail.com>
2026-07-19 13:51:17 +0545

    Partial_Class

--- J_partial_class/c_partial_class_3/Program.cs
+++ J_partial_class/c_partial_class_3/Program.cs
@@ -4,8 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Employee e =new Employee(101,"Mina");
-        e.Salary=3400;
+        Employee e = new Employee(101, "Mina");
+        e.Salary = 3400;
         e.Display();
     }
 }
