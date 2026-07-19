2491e3d823f974df8e9c47320c8b286046172db5
budhathokidai <budhathokirabi07@gmail.com>
2026-07-19 13:49:23 +0545

    Type_Conversion

--- G_type_conversion/c_non_compatibal/Program.cs
+++ G_type_conversion/c_non_compatibal/Program.cs
@@ -4,12 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        string  a="123";
-        int b=Convert.ToInt32(a);          //int b=(int)a (its not possible)
+        string a = "123";
+        int b = Convert.ToInt32(a);          //int b=(int)a (its not possible)
         //int b=int.Parse(a);  Parse method
-        int c=354;
-        int sum=b+c;
-        Console.WriteLine("Sum="+sum);
+        int c = 354;
+        int sum = b + c;
+        Console.WriteLine("Sum=" + sum);
     }
 }
 
