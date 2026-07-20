6a8d706923c94951cd78782aef564bc620eb89f0
budhathokidai <budhathokirabi07@gmail.com>
2026-07-20 07:23:18 +0545

    Inheritance

---
+++ L_inheritance/c_inheritance_multilevel/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace c_inheritance_multilevel;
+class Shop
+{
+    public void step_1()
+    {
+        Console.WriteLine("This is Shop!");
+    }
+}
+class Shopkeeper : Shop
+{
+    public void step_2()
+    {
+        Console.WriteLine("Shopkeeper can acces the shop");
+    }
+}
+class Product : Shopkeeper
+{
+    public void step_3()
+    {
+        Console.WriteLine("Shopkeeper can only access the product!");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Product p=new Product();
+        p.step_1();
+        p.step_2();
+        p.step_3();
+        
+    }
+}
+/*This is Shop!
+Shopkeeper can acces the shop
+Shopkeeper can only access the product!*/
