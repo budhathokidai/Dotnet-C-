d4af57493868e8b5504497d44678c4aae20012f3
budhathokidai <budhathokirabi07@gmail.com>
2026-07-21 10:44:54 +0545

    Interface

---
+++ P_interface/f_interface_same_method/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace f_interface_same_method;
+//Explicit Interface Implementation
+interface ITeacher
+{
+    void Show();
+}
+
+interface IStudent
+{
+    void Show();
+}
+
+class Person : ITeacher, IStudent
+{
+    void IStudent.Show()
+    {
+        Console.WriteLine("I am a Student.");
+    }
+    void ITeacher.Show()
+    {
+        Console.WriteLine("I am a Teacher.");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Person p = new Person();
+        ITeacher t1=p;
+        IStudent s1=p;
+        t1.Show();
+        s1.Show();
+    }
+}
+/*I am a Teacher.
+I am a Student.*/
