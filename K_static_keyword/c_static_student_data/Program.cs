5be3d6236f856c403977fce051eb49162226cc0d
budhathokidai <budhathokirabi07@gmail.com>
2026-07-21 10:42:56 +0545

    Static_Keyword

---
+++ K_static_keyword/c_static_student_data/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace c_static_student_data;
+
+static class Student
+{
+    public static string fname="Rabi";
+    public static string lname="Budhathoki";
+    public static int age=21;
+    public static void display()
+    {
+        Console.WriteLine("Details of Student");
+
+    }
+
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Student.display();
+        Console.WriteLine("First name= "+Student.fname);
+        Console.WriteLine("Last name= "+Student.lname);
+        Console.WriteLine("Age= "+Student.age);
+    }
+}
