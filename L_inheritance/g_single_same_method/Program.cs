c45819092c53e856593570b0265bac3cf9c8788a
budhathokidai <budhathokirabi07@gmail.com>
2026-07-21 10:43:57 +0545

    Inheritance

---
+++ L_inheritance/g_single_same_method/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace g_single_same_method;
+class SuperClass
+{
+    public void hello()
+    {
+        Console.WriteLine("Hello ma chai superclass");
+    }
+}
+class SubClass : SuperClass
+{
+    public new void hello()
+    {
+        Console.WriteLine("Hello ma chai subclass");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        SubClass s1 = new SubClass();
+        SuperClass s2 = new SubClass();
+        s1.hello();
+        s2.hello();
+    }
+}
+/*Hello ma chai subclass
+Hello ma chai superclass*/
