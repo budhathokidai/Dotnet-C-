06433490a26810114c7991974aa5500fe44d6141
budhathokidai <budhathokirabi07@gmail.com>
2026-07-21 10:44:39 +0545

    Interface

---
+++ P_interface/e_interface_with_property/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace e_interface_with_property;
+#nullable disable
+interface IStudent
+{
+    string Name { get;set;}
+    void show();
+}
+class Student : IStudent
+{
+    public string Name { get; set; }
+
+    public void show()
+    {
+        Console.WriteLine("Student Name: " + Name);
+    }   
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Student s = new Student();
+        s.Name = "Rabi";
+        s.show();
+    }
+}
+/*Student Name: Rabi*/
