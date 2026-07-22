1f4c99d05df6d33300241bb1b3fc13b943a483e7
budhathokidai <budhathokirabi07@gmail.com>
2026-07-22 18:27:26 +0545

    Exception_handling

---
+++ Q_exception_handaling/b_exception_handel/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace b_exception_handel;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a=10;
+        int b=0;
+        try
+        {
+        int output=a/b;
+        Console.WriteLine("Answer is :"+output);   
+        }
+        catch (ArithmeticException e)
+        {
+            Console.WriteLine("A cannot be divided by b"+e);
+        }
+    }
+}
