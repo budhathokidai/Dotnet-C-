8648160041643d744bb178a0a565daaf4f291ba5
budhathokidai <budhathokirabi07@gmail.com>
2026-07-22 18:27:06 +0545

    Exception_handling

---
+++ Q_exception_handaling/a_exception_scenario/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace a_exception_scenario;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a=10;
+        int b=0;
+        int output=a/b;
+        Console.WriteLine("Answer is :"+output);
+    }
+}
