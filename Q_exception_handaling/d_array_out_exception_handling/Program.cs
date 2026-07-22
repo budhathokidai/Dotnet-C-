de914d9dcf33d0c40f29c4f7576fd365a0107ee7
budhathokidai <budhathokirabi07@gmail.com>
2026-07-22 18:27:53 +0545

    Exception_handling

---
+++ Q_exception_handaling/d_array_out_exception_handling/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace d_array_out_exception_handling;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] nums = new int[5];
+        try
+        { 
+            nums[10] = 30;
+            Console.WriteLine(nums[10]);
+        }
+        catch (Exception e)
+        //catch(IndexOutOfRangeException.e)
+        {
+            Console.WriteLine("Out of range!" + e);
+        }
+
+    }
+}
