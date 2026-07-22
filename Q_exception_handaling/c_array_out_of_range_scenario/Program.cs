3d773fade164271fba21dba23c385dc409877f8e
budhathokidai <budhathokirabi07@gmail.com>
2026-07-22 18:27:43 +0545

    Exception_handling

---
+++ Q_exception_handaling/c_array_out_of_range_scenario/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace c_array_out_of_range_scenario;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] nums=new int[5];
+        nums[10]=30;
+        Console.WriteLine(nums[10]);
+    }
+}
