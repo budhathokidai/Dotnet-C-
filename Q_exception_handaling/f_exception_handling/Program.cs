5cb0df573e84e516dfa2d2b9f7f59abd44b48c75
budhathokidai <budhathokirabi07@gmail.com>
2026-07-23 19:04:11 +0545

    Exception_Handling

---
+++ Q_exception_handaling/f_exception_handling/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace f_exception_handling;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //string value=null;
+        string name ="Rabi";
+        try
+        {
+            //if(name.Length==0)
+            if(name.Length != 0)
+            {
+                Console.WriteLine(name);
+            }
+        }
+        catch(Exception e)
+        {
+           Console.WriteLine("Exception Happened"+e);  
+        }
+       
+    }
+}
