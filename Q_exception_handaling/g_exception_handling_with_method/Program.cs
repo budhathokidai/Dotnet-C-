c8f062c99dbb043e14c0867a3b6620a9af068f44
budhathokidai <budhathokirabi07@gmail.com>
2026-07-23 19:04:20 +0545

    Exception_Handling

---
+++ Q_exception_handaling/g_exception_handling_with_method/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace g_exception_handling_with_method; 
+class Calculator
+{
+    public int Modulus(int num1, int num2)
+    {
+        return num1 % num2;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Calculator cal = new Calculator();
+
+        int number1 = 25;
+        int number2 = 4;
+
+        try
+        {
+            int remainder = cal.Modulus(number1, number2);
+            Console.WriteLine("The remainder is: " + remainder);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("An error occurred: " + ex.Message);
+        }
+    }
+}
+//The remainder is: 1
