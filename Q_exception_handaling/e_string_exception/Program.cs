4cec007b826fcbeb3d7904ab75071adc529d8b9e
budhathokidai <budhathokirabi07@gmail.com>
2026-07-23 19:04:02 +0545

    Exception_Handling

---
+++ Q_exception_handaling/e_string_exception/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace e_string_exception;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] names = { "Rabi", "Ram", "Rewon" };
+        try
+        {
+            //Console.WriteLine(names[2]);
+            Console.WriteLine(names[3]);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Exception found " + e);
+        }
+    }
+}
