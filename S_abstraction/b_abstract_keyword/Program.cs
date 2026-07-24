01740a0e46f1f07953ab73a8be38bb0525766007
budhathokidai <budhathokirabi07@gmail.com>
2026-07-24 08:59:26 +0545

    Abstarction

---
+++ S_abstraction/b_abstract_keyword/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace b_abstract_keyword;
+abstract class Nums
+{
+    public virtual int add(int a,int b)
+    {
+        return (a+b);
+    }
+}
+class Calc : Nums
+{
+    public override int add(int a,int b)
+    {
+        return (a+b);
+    }
+    
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Calc c=new Calc();
+        int result=c.add(90,90);
+       Console.WriteLine("Sum="+result);
+    }
+}
