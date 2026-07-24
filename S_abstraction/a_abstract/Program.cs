b92740e4ac8a95496ab3a4723e07e283f98d4bea
budhathokidai <budhathokirabi07@gmail.com>
2026-07-24 08:59:13 +0545

    Abstarction

---
+++ S_abstraction/a_abstract/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace a_abstract;
+abstract class Nepal
+{
+   public abstract void hlo(); 
+}
+class China: Nepal
+{
+    public override void hlo()
+    {
+        Console.WriteLine("I am chinese!");
+    }
+}
+class India : Nepal
+{
+    public override void hlo()
+    {
+        Console.WriteLine("I am indian!");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Nepal n;  //not object just a refernce
+        n=new China();
+        n.hlo();
+        n=new India();
+        n.hlo();
+
+    }
+}
+
+/*I am chinese!
+I am indian!*/
