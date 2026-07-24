ddadb93e4ba2a9f2cfa89493da1e120259b80fe5
budhathokidai <budhathokirabi07@gmail.com>
2026-07-24 08:57:51 +0545

    Virtual_Method

---
+++ R_virtual_method/a_virtual_method/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace a_virtual_method;
+
+class SuperClass
+{
+    public virtual int calc(int a,int b)
+    {
+        return(a+b);
+    }
+}
+class SubClass : SuperClass
+{
+    public override int calc(int a, int b)
+    {
+        return (a+b+4);
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        SubClass obj=new SubClass();
+        Console.WriteLine("Redefined result="+obj.calc(1,2));
+    }
+}
+
+/*Redefined result=7*/
