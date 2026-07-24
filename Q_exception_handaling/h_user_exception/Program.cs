2e6adae9436d19c7b0da0f84afc85f64ccd80dcb
budhathokidai <budhathokirabi07@gmail.com>
2026-07-24 08:33:19 +0545

    Exception_Handling

---
+++ Q_exception_handaling/h_user_exception/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace h_user_exception;
+
+class MyExeption : Exception
+{
+    public MyExeption(string str)
+    {
+        Console.WriteLine(str);
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+
+    {
+        MyExeption obj = new MyExeption("This is user defiened exception");
+        try
+        {
+            throw obj;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+
+        }
+
+    }
+}
