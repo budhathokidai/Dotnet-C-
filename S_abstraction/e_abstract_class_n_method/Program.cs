8388aafd158c7897a2e16961602f6d7d718659aa
budhathokidai <budhathokirabi07@gmail.com>
2026-07-27 20:29:43 +0545

    Abstract_Keyword

---
+++ S_abstraction/e_abstract_class_n_method/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace e_abstract_class_n_method;
+
+abstract class Nepal
+{
+    public abstract void hel();
+}
+class Lalitpur : Nepal
+{
+    public override void hel()
+    {
+        Console.WriteLine("Ma chai lalitpur bata hai");
+    }
+}
+class Bhaktapur : Nepal
+{
+    public override void hel()
+    {
+        Console.WriteLine("Ma chai Bhaktapur bata hai");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Nepal n;
+        n = new Lalitpur();
+        n.hel();
+        n = new Bhaktapur();
+        n.hel();
+    }
+}
+
+/*Ma chai lalitpur bata hai
+Ma chai Bhaktapur bata hai*/
