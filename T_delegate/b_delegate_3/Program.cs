358bbee3ab0699c2b69c60f9fb6dd9260ae7f136
budhathokidai <budhathokirabi07@gmail.com>
2026-07-30 20:55:54 +0545

    Delegate

---
+++ T_delegate/b_delegate_3/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace b_delegate_3;
+
+public delegate void MyDel(string nmae);
+
+class Program
+{
+
+    public static void displayName(String name)
+    {
+        Console.WriteLine("Hello " + name);
+
+    }
+    static void Main(string[] args)
+    {
+        MyDel d1 = new MyDel(displayName);
+        d1("samothimi");
+    }
+}
