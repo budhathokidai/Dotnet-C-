a880c105f73015f91214535807fa81a5208de84c
budhathokidai <budhathokirabi07@gmail.com>
2026-07-30 20:55:25 +0545

    Delegate

---
+++ T_delegate/a_delegate_2/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace a_delegate_2;
+
+public delegate void MyDel(int num);
+
+class MyClass
+{
+    public static void square(int num)
+    {
+        Console.WriteLine("Square of {0} is {1}", num, num * num);
+    }
+    public static void cube(int num)
+    {
+        Console.WriteLine("Square of {0} is {1}", num, num * num);
+    }
+}
+class Program : MyClass
+{
+    static void Main(string[] args)
+    {
+        MyDel d1 = square;
+        d1(3);
+        MyDel d2 = square;
+        d2(4);
+    }
+}
+
+/*Square of 3 is 9
+Square of 4 is 16*/
