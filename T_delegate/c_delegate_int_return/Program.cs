795b5a2b89b31b1c1091a960a7debeb09fb42627
budhathokidai <budhathokirabi07@gmail.com>
2026-07-30 20:56:08 +0545

    Delegate

---
+++ T_delegate/c_delegate_int_return/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace c_delegate_int_return;
+
+public delegate int NumCalc(int num);
+
+class Program
+{
+    public static int num=10;
+    public static int addnum(int a)
+    {
+        num+=a;
+        return num;
+    }
+    public static int mulnum(int a)
+    {
+        num*=a;
+        return num;
+    }
+     public static int getnum()
+    {
+        return num;
+    }
+    static void Main(string[] args)
+    {
+        NumCalc n1= new NumCalc(addnum);
+        n1(5);
+        Console.WriteLine("New value is = {0}",getnum());
+        NumCalc n2= new NumCalc(mulnum);
+        n2(5);
+        Console.WriteLine("New value is {0}",getnum());
+    } 
+}
+/*New value is = 15
+New value is 75*/
+
