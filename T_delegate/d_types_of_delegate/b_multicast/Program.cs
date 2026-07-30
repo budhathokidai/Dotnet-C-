6050931727ff18c9211a5bb7db4b394ac1c98442
budhathokidai <budhathokirabi07@gmail.com>
2026-07-30 20:56:47 +0545

    Delegate

---
+++ T_delegate/d_types_of_delegate/b_multicast/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace b_multicast;  //nonstatic
+public delegate void MyDel(int a,int b);  //bith static and non static in delegate
+class Calculation
+{
+    public  void add(int a,int b)
+    {
+        Console.WriteLine("The sum="+(a+b));
+    }
+    public  void mul(int a,int b)
+    {
+        Console.WriteLine("The Product="+(a*b));
+    }
+    static void Main(string[] args)
+    {
+        Calculation cobj=new Calculation();
+        MyDel d1=new MyDel(cobj.add);
+        MyDel d2=new MyDel(cobj.mul);
+        d1=d1+d2;
+        d1(20,30);
+    }
+}
