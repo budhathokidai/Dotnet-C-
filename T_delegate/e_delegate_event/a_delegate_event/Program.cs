44fbe72bb1f140cb4b6c49ddbec93b8d2a120989
budhathokidai <budhathokirabi07@gmail.com>
2026-07-31 20:40:44 +0545

    Delegate

---
+++ T_delegate/e_delegate_event/a_delegate_event/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace a_delegate_event;
+public delegate string Mydel(string str);
+class Eventdel
+{
+    event Mydel MyEvent;
+    public Eventdel()
+    {
+        this.MyEvent +=new Mydel(this.DisplayMessage);
+    }
+    public string DisplayMessage(string username)
+    {
+        return "Hello " +username;
+    }
+    static void Main(string[] args)
+    {
+        Eventdel obj =new Eventdel();
+        string result =obj.MyEvent("Sanothimi");
+        Console.WriteLine(result);
+    }
+}
+/*Hello Sanothimi*/
---
+++ T_delegate/e_delegate_event/b_delegate_event_2/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace b_delegate_event_2;
+#nullable disable
+//1:define a delegate
+
+public delegate void TrafficLightChangeHandler(string color);  
+
+public class TrafficLight
+{
+    //declare event (event is built in refernce)
+    public event TrafficLightChangeHandler TrafficLightChanged;
+    public void ChangeLight(string color)
+    {
+        //method that raised the event 
+        Console.WriteLine("The Traffic Light is "+color);
+        TrafficLightChanged.Invoke(color);
+    }
+}
+public class Car
+    {
+        public void ReactToLight(string lightcolor)
+    {
+        //event handling method
+        if (lightcolor == "Red")
+        {
+            Console.WriteLine("Car stops.");
+        }else if(lightcolor == "Green")
+        {
+            Console.WriteLine("Car starts moving.");
+        }
+    }  
+    }
+class MainProgram
+{
+    static void Main(string[] args)
+    {
+        TrafficLight light=new TrafficLight();
+        Car car=new Car();
+        //subscribe to the event
+        light.TrafficLightChanged +=car.ReactToLight;
+        // Trigger the event
+        light.ChangeLight("Green");
+        light.ChangeLight("Red");
+    }
+}
