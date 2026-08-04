bbfef1e3e2429e5af70461a37e5f02a2a12be666
budhathokidai <budhathokirabi07@gmail.com>
2026-08-04 10:41:34 +0545

    String Manipulation

---
+++ V_String_manipulation/h_str_concat_function/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace h_str_concat_function;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //function concatinate
+        string surukoname = "Sandy";
+        string lastnam = "Shrestha";
+        Console.WriteLine(string.Concat(surukoname,lastnam));
+    }
+}
