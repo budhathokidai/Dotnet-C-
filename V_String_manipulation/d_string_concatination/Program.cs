62b5e971dcfee280e2eabba656b74283bb674cee
budhathokidai <budhathokirabi07@gmail.com>
2026-08-04 10:40:52 +0545

    String Manipulation

---
+++ V_String_manipulation/d_string_concatination/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace d_string_concatination;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // string concatenation
+        string fname = "Rabi";
+        string lname = "Budhathoki";
+        string fullname = fname + " " + lname;
+        Console.WriteLine(fullname);
+    }
+}
