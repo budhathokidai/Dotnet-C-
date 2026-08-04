1a60f52dbf23d39647c3f727e562d1294a5649e0
budhathokidai <budhathokirabi07@gmail.com>
2026-08-04 21:26:35 +0545

    String Manipulation

---
+++ V_String_manipulation/k_str_copy/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace k_str_copy;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string name = "Rabi";
+        // string myname = string.Copy(name); // not recommended to use
+        string myname= new string(name);
+        Console.WriteLine("Source string =" + name);
+        Console.WriteLine("Copied string =" + myname);
+    }
+}
+
+/*Source string = Rabi
+Copied string = Rabi*/
