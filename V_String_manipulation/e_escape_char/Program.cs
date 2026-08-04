4eecd04ed5f2a98b2e2f7a0693c662b6ba0f7b19
budhathokidai <budhathokirabi07@gmail.com>
2026-08-04 10:41:00 +0545

    String Manipulation

---
+++ V_String_manipulation/e_escape_char/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace e_escape_char;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // escape characters
+        // 1. this will print "Rabi" in the next line.
+        Console.WriteLine("Hello\nRabi");
+
+        // 2. this will privide a tab between the words.
+        Console.WriteLine("Hello\tRabi");
+
+        // 3. lets you to print something with double quote
+        Console.WriteLine("\"Hello Rabi\"");
+
+        // 4. to print path using slash
+        Console.WriteLine("D:\\SEM6\\C#\\");
+    }
+}
