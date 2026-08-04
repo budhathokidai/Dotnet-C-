46ec159f8f3f2317de996935669b3cfe882ee02e
budhathokidai <budhathokirabi07@gmail.com>
2026-08-04 10:41:22 +0545

    String Manipulation

---
+++ V_String_manipulation/g_str_length/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace g_str_length;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //string length
+        string clg = "Sanothimi";
+        int len = clg.Length;
+        Console.WriteLine("Character length of {0} is ={1}", clg);
+    }
+}
