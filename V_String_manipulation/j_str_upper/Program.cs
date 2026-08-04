ac6c550de7c108d24f64a4499e4232a1f22fbe75
budhathokidai <budhathokirabi07@gmail.com>
2026-08-04 10:41:57 +0545

    String Manipulation

---
+++ V_String_manipulation/j_str_upper/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace j_str_upper;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string str1 = "Nanu";
+        string str2 = str1.ToUpper();
+        Console.WriteLine("Original string = " + str1);
+        Console.WriteLine("Converted lowercase string = " + str2);
+    }
+}
