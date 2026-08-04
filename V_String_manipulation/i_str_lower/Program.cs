721fdb038a129977244ca618825f8df6a5b66dad
budhathokidai <budhathokirabi07@gmail.com>
2026-08-04 10:41:49 +0545

    String Manipulation

---
+++ V_String_manipulation/i_str_lower/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace i_str_lower;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string str1 = "Sanu";
+        string str2 = str1.ToLower();
+        Console.WriteLine("Original string = " + str1);
+        Console.WriteLine("Converted lowercase string = " + str2);
+    }
+}
