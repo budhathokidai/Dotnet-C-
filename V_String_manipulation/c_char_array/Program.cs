160cfc6d3db384d9b4943cffbede563b9ccdccfa
budhathokidai <budhathokirabi07@gmail.com>
2026-08-04 10:40:44 +0545

    String Manipulation

---
+++ V_String_manipulation/c_char_array/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace c_char_array;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+         // array of characters
+        char[] namearray = { 'R', 'a', 'b','i' };
+        string name = new string(namearray);
+        Console.WriteLine(name);
+    }
+}
