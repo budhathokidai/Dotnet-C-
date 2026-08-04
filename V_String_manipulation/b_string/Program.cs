d16b3730fd520a326806955ff57a5f6dfd64df8b
budhathokidai <budhathokirabi07@gmail.com>
2026-08-04 10:40:36 +0545

    String Manipulation

---
+++ V_String_manipulation/b_string/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace b_string;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+         // printing string within double quote
+        Console.WriteLine("Rabi");
+
+        // printing string from variable
+        string name = "Rabi";
+        Console.WriteLine(name);
+    }
+}
