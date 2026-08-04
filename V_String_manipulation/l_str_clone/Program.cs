bb68d336256aaa9e550d00afa45b5b2d370924f7
budhathokidai <budhathokirabi07@gmail.com>
2026-08-04 21:26:47 +0545

    String Manipulation

---
+++ V_String_manipulation/l_str_clone/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace l_str_clone;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string str1 = "Rabi";
+        string str2 = (string)str1.Clone();
+        Console.WriteLine("Source string = " + str1);
+        Console.WriteLine("Cloned string = " + str2);
+    }
+}
+/*Source string = Rabi
+Cloned string = Rabi*/
