5b534036036e1498f2cc9e378d6e67a30f884f40
budhathokidai <budhathokirabi07@gmail.com>
2026-08-04 21:27:27 +0545

    String Manipulation

---
+++ V_String_manipulation/o_str_equals/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace o_str_equals;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string name1 = "Rabi";
+        string name2 = "Hari";
+        string name3 = "Rabi";
+        
+        //The Equals() method checks whether two strings have the same sequence of characters.
+        bool result1 = name1.Equals(name2);
+        bool result2 = name1.Equals(name3);
+
+        Console.WriteLine(result1);
+        Console.WriteLine(result2);
+    }
+}
+
+/*False  //They are different, so the result is False.
+True    //They are the same, so the result is True.
+*/
