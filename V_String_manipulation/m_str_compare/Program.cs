3758125afedbcbe8401886f62684f28361fd205a
budhathokidai <budhathokirabi07@gmail.com>
2026-08-04 21:26:55 +0545

    String Manipulation

---
+++ V_String_manipulation/m_str_compare/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace m_str_compare;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string s1 = "Rabi";
+        string s2 = "Budhathoki";
+        string s3 = "rabi";
+        string s4 = "Rabi";
+
+        Console.WriteLine(string.Compare(s1, s2));
+        Console.WriteLine(string.Compare(s2, s3));
+        Console.WriteLine(string.Compare(s1, s4)); // 0 (both strings are equal)
+    }
+}
