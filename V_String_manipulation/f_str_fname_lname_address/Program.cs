0bfda0c205be8356f03a056f30617cede1d8934b
budhathokidai <budhathokirabi07@gmail.com>
2026-08-04 10:41:11 +0545

    String Manipulation

---
+++ V_String_manipulation/f_str_fname_lname_address/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace f_str_fname_lname_address;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string fnaame = "Heri";
+        string lnaame = "Bahadur";
+        string address = "KTM";
+        Console.WriteLine(fnaame + " " + lnaame + "lives in" + address);
+        Console.WriteLine("{0} {1} lives in {2}", fnaame, lnaame, address);
+    }
+}
