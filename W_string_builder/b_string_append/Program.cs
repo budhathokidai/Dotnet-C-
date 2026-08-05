78789d3ca020579128fcf5aa9257818a16f5f05c
budhathokidai <budhathokirabi07@gmail.com>
2026-08-05 20:21:55 +0545

    String_Builder

---
+++ W_string_builder/b_string_append/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Text;
+
+namespace b_string_append;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        StringBuilder s=new StringBuilder("Hello rabi ");
+        //s,.Append(9.9);
+        s.AppendFormat("{0:c}",9.9);
+        Console.WriteLine(s);
+    }
+}
+/*Hello rabi £9.90*/
