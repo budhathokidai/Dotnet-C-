dc46ea81e3d4fb5866e10b97121dc259228f42f4
budhathokidai <budhathokirabi07@gmail.com>
2026-08-05 20:22:08 +0545

    String_Builder

---
+++ W_string_builder/c_string_builder/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+namespace c_string_builder;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        const int MaxSize = 50;
+        StringBuilder s = new StringBuilder();
+        void AppendwithLimit(string text)
+        {
+            if (s.Length + text.Length > MaxSize)
+            {
+                Console.WriteLine("Cnnot append: Size limit reached!");
+                return;
+            }
+            s.Append(text);
+        }
+        AppendwithLimit("Hello ");
+        AppendwithLimit(" Rabi ");
+        AppendwithLimit(new string('x', 10));
+        AppendwithLimit(new string('x', 40));
+        Console.WriteLine(s.ToString());
+
+
+    }
+}
