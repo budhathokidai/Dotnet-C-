b2e2ff4cfb93c871b928e72ebf6ecf112f689032
budhathokidai <budhathokirabi07@gmail.com>
2026-08-06 21:51:28 +0545

    String Builder

---
+++ W_string_builder/f_string_builder_replace/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+
+namespace f_string_builder_replace;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        StringBuilder s= new StringBuilder("Hello, Bhaktapur");
+
+        s.Replace("Bhaktapur", "Sanothimi");
+
+        Console.WriteLine(s);
+    }
+}
+
+/*Hello, Sanothimi*/
