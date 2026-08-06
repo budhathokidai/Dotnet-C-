eddc36135172b1646e04f6207caae9700bcffee9
budhathokidai <budhathokirabi07@gmail.com>
2026-08-06 21:58:05 +0545

    Collection_List

---
+++ X_Collection/A_Gneric_Collection/A_List/o_List_Copy/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace o_List_Copy;
+//Copies one list into another.
+class Program
+{
+    static void Main(string[] args)
+    { var nums = new List<int>()
+        {
+            12,
+            3,
+            4,
+            45,
+            32
+        };
+
+        List<int> copy = new List<int>(nums);
+
+        copy.ForEach(x => Console.WriteLine(x));
+    }
+}
