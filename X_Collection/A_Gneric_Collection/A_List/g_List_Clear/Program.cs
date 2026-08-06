db7f53b580453bdf87224e618ceed9b21a942f9f
budhathokidai <budhathokirabi07@gmail.com>
2026-08-06 21:55:09 +0545

    Collection_List

---
+++ X_Collection/A_Gneric_Collection/A_List/g_List_Clear/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace g_List_Clear;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       var nums = new List<int>()
+        {
+            12,
+            3,
+            4,
+            45,
+            32
+        };
+
+        nums.Clear();
+    }
+}
