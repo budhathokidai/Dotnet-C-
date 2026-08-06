3ae06121628f6dfbdf828613006da1e0441a341d
budhathokidai <budhathokirabi07@gmail.com>
2026-08-06 21:54:52 +0545

    Collection_List

---
+++ X_Collection/A_Gneric_Collection/A_List/f_List_RemoveAt/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace f_List_RemoveAt;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var nums = new List<int>()
+        {
+            12,
+            3,
+            4,
+            45,
+            32
+        };
+
+        nums.RemoveAt(2);  //indexing Remove
+
+        nums.ForEach(x => Console.WriteLine(x));
+    }
+}
+
+
+/*12
+3
+45
+32*/
