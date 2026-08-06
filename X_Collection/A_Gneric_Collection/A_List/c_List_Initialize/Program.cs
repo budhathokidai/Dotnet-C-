a794ee3cd5ddaae0ce857eb15f65a7395e8fc2ed
budhathokidai <budhathokirabi07@gmail.com>
2026-08-06 21:53:56 +0545

    Collection_List

---
+++ X_Collection/A_Gneric_Collection/A_List/c_List_Initialize/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace c_List_Initialize;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+         var nums = new List<int>()
+        {
+            23,
+            32,
+            3,
+            4
+        };
+
+        Console.WriteLine(nums[0]);
+        Console.WriteLine(nums[1]);
+        Console.WriteLine(nums[2]);
+        Console.WriteLine(nums[3]);
+    }
+}
+
+/*23
+32
+3
+4*/
