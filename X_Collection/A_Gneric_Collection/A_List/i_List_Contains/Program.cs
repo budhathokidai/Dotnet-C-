8ecc40079e948105fdbcaafe6f6756666bbddcca
budhathokidai <budhathokirabi07@gmail.com>
2026-08-06 21:56:22 +0545

    Collection_List

---
+++ X_Collection/A_Gneric_Collection/A_List/i_List_Contains/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace i_List_Contains;
+//Checks whether a specific element exists in the list.
+class Program
+{
+    static void Main(string[] args)
+    {
+        var nums = new List<int>()
+        {
+            12,
+            3,
+            4,
+            45,
+            32
+        };
+
+        Console.WriteLine(nums.Contains(45));
+        Console.WriteLine(nums.Contains(100));
+    }
+}
+
+/*True
+False*/
