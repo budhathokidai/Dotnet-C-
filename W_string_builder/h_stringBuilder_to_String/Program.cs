de0adac356aa365efa5bb26c8e3aa7b3042c5894
budhathokidai <budhathokirabi07@gmail.com>
2026-08-06 21:51:43 +0545

    String Builder

---
+++ W_string_builder/h_stringBuilder_to_String/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace h_stringBuilder_to_String;
+using System.Text;
+class Program
+{
+    static void Main(string[] args)
+    {
+        StringBuilder s = new StringBuilder("Hello Sanothimi");
+
+        string str = s.ToString();
+
+        Console.WriteLine(str);
+    }
+}
+
+/*Hello Sanothimi*/
