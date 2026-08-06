8e35ef1c5f1993b9a907011ace6976ad87ddf061
budhathokidai <budhathokirabi07@gmail.com>
2026-08-06 21:56:44 +0545

    Collection_List

---
+++ X_Collection/A_Gneric_Collection/A_List/j_List_Sort/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace j_List_Sort;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+         var nums = new List<int>()
+        {
+            45,
+            12,
+            3,
+            32,
+            4
+        };
+
+        nums.Sort();
+
+        nums.ForEach(x => Console.WriteLine(x));
+
+    }
+}
+/*
+3
+4
+12
+32
+45*/
