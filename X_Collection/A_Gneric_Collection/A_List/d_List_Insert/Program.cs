8e5b83f83c53a4c560fdc183eeddf0388eb65b5a
budhathokidai <budhathokirabi07@gmail.com>
2026-08-06 21:54:12 +0545

    Collection_List

---
+++ X_Collection/A_Gneric_Collection/A_List/d_List_Insert/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace d_List_Insert;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var nums = new List<int>()
+        {
+            12,
+            3,
+            4,
+            45,
+            32
+        };
+
+        nums.Insert(0, 34);
+
+        nums.ForEach(x => Console.WriteLine(x));
+    }
+}
+
+/*34
+12
+3
+4
+45
+32*/
