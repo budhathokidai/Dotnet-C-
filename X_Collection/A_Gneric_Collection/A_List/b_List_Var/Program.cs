0e7a8402f1672b44ffa3d837793093eb5fdb1905
budhathokidai <budhathokirabi07@gmail.com>
2026-08-06 21:53:42 +0545

    Collection_List

---
+++ X_Collection/A_Gneric_Collection/A_List/b_List_Var/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace b_List_Var;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var nums = new List<int>();
+
+        nums.Add(34);
+        nums.Add(3);
+        nums.Add(23);
+
+        Console.WriteLine(nums[0]);
+        Console.WriteLine(nums[1]);
+        Console.WriteLine(nums[2]);
+    }
+}
+
+
+/*34
+3
+23*/
