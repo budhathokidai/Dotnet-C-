5556c50f3ad51364a31464d6bdf7e85794e0abbf
budhathokidai <budhathokirabi07@gmail.com>
2026-08-06 21:57:54 +0545

    Collection_List

---
+++ X_Collection/A_Gneric_Collection/A_List/n_List_FindAll/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace n_List_FindAll;
+//Finds all elements that satisfy a condition.
+class Program
+{
+    static void Main(string[] args)
+    {
+        var nums = new List<int>()
+        {
+            12,
+            3,
+            4,
+            45,
+            32,
+            22,
+            15
+        };
+
+        var result = nums.FindAll(x => x > 20);
+
+        result.ForEach(x => Console.WriteLine(x));  //Print every element in the result list one by one.
+    }
+}
+
+/*
+45
+32
+22*/
