6127448c8bb9a6af73f81609b793aa1423f127d6
budhathokidai <budhathokirabi07@gmail.com>
2026-08-06 21:51:00 +0545

    String Builder

---
+++ W_string_builder/d_string_insert/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Text;
+
+namespace d_string_insert;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        StringBuilder s = new StringBuilder("Hello",20);
+         s.Insert(2, "Rabi");
+        Console.WriteLine(s);
+    }
+}
+
+/*HeRabillo*/
