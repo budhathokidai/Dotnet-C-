26a79e41adeaa294f050f1fdcb0026e13013e257
budhathokidai <budhathokirabi07@gmail.com>
2026-08-06 21:56:55 +0545

    Collection_List

---
+++ X_Collection/A_Gneric_Collection/A_List/k_List_Reverse/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace k_List_Reverse;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var nums = new List<int>()
+        {
+            12,
+            3,
+            4,
+            45,
+            32
+        };
+
+        nums.Reverse();
+
+        nums.ForEach(x => Console.WriteLine(x));
+    }
+}
+
+
+/*32
+45
+4
+3
+12*/
