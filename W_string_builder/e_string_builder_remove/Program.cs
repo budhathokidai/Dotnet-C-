d23a09c1644252cec865878182094f9e5b44ea34
budhathokidai <budhathokirabi07@gmail.com>
2026-08-06 21:51:18 +0545

    String Builder

---
+++ W_string_builder/e_string_builder_remove/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+
+namespace e_string_builder_remove;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       StringBuilder s= new StringBuilder("Hello, Welcome Rabi Budhathoki", 30);
+
+        s.Remove(5, 9); // Means start removing from index 5 Remove 9 characters
+
+        Console.WriteLine(s);
+    }
+}
+
+/*Hello Rabi Budhathoki*/
