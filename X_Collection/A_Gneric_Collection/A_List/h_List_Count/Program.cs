daab850fc1736d4e8ec95a1f8ac50b77acd16de8
budhathokidai <budhathokirabi07@gmail.com>
2026-08-06 21:55:26 +0545

    Collection_List

---
+++ X_Collection/A_Gneric_Collection/A_List/h_List_Count/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace h_List_Count;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var nums = new List<int>()
+        {
+            12,
+            3,
+            4,
+            45,
+            32
+        };
+
+        Console.WriteLine("Total Elements = " + nums.Count);
+    }
+}
+
+/*Total Elements = 5*/
