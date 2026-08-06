2fa3465f143e7f6a56237443a648bd041eca4412
budhathokidai <budhathokirabi07@gmail.com>
2026-08-06 21:57:21 +0545

    Collection_List

---
+++ X_Collection/A_Gneric_Collection/A_List/m_List_Find/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace m_List_Find;
+//Finds the first element that matches a condition.
+class Program
+{
+    static void Main(string[] args)
+    {
+         var nums = new List<int>()
+        {
+            12,
+            3,
+            4,
+            45,
+            32
+        };
+
+        int result = nums.Find(x => x > 20);
+
+        Console.WriteLine(result);
+    }
+}
+
+/*
+45*/
