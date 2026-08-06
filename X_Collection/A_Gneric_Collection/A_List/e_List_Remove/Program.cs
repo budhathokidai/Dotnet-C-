9826607dafa77c2f280ccf79d9654af6502b6a37
budhathokidai <budhathokirabi07@gmail.com>
2026-08-06 21:54:35 +0545

    Collection_List

---
+++ X_Collection/A_Gneric_Collection/A_List/e_List_Remove/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace e_List_Remove;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+         var nums = new List<int>()
+        {
+            12,
+            3,
+            4,
+            45,
+            32
+        };
+
+        nums.Remove(45);
+
+        nums.ForEach(x => Console.WriteLine(x));
+    }
+}
+
+
+/*12
+3
+4
+32*/
