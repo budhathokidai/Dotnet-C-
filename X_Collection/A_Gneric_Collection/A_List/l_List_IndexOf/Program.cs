6e634a6567543583c9acdc48ffa6591cbf1dd16e
budhathokidai <budhathokirabi07@gmail.com>
2026-08-06 21:57:08 +0545

    Collection_List

---
+++ X_Collection/A_Gneric_Collection/A_List/l_List_IndexOf/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace l_List_IndexOf;
+//Returns the index of an element.
+class Program
+{
+    static void Main(string[] args)
+    {
+        var nums = new List<int>()
+        {
+            12,
+            3,
+            4,
+            45,
+            32
+        };
+
+        Console.WriteLine(nums.IndexOf(45));
+        Console.WriteLine(nums.IndexOf(100));
+        Console.WriteLine(nums.IndexOf(12));
+    }
+}
+
+/*3
+-1
+0*/
