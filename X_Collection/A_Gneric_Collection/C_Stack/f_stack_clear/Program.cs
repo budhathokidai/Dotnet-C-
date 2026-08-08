258ebe60cd2b7ede4f52054f36463442c975790d
budhathokidai <budhathokirabi07@gmail.com>
2026-08-08 20:44:40 +0545

    Collection_Stack

---
+++ X_Collection/A_Gneric_Collection/C_Stack/f_stack_clear/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace f_stack_clear;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Stack<int> myNums = new Stack<int>();
+
+        myNums.Push(23);
+        myNums.Push(4);
+        myNums.Push(6);
+
+        myNums.Clear();  //Clear() removes all elements from the stack.
+
+        Console.WriteLine("Total elements = " + myNums.Count);
+    }
+}
+
+
+/*Total elements = 0*/
