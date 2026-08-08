f06c6819afb98b67a2b31cb37cd7a3287d56748d
budhathokidai <budhathokirabi07@gmail.com>
2026-08-08 20:41:34 +0545

    Collection_Queue

---
+++ X_Collection/A_Gneric_Collection/B_queue/d_queue_count/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace d_queue_count;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Queue<int> nums = new Queue<int>();
+        nums.Enqueue(23);
+        nums.Enqueue(22);
+        nums.Enqueue(26);
+        nums.Enqueue(76);
+
+        Console.WriteLine("The number of elements in the queue: " + nums.Count);
+        foreach (var item in nums)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
