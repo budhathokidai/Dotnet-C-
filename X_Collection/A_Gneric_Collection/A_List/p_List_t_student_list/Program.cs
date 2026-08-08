e0ee52cd7308ca34e38452b4692648b7c3c8c615
budhathokidai <budhathokirabi07@gmail.com>
2026-08-08 20:39:06 +0545

    List

---
+++ X_Collection/A_Gneric_Collection/A_List/p_List_t_student_list/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.InteropServices;
+#nullable disable
+namespace p_List_t_student_list;
+
+public class Student
+{
+    public int roll { get; set; }
+    public string name { get; set; }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        var students = new List<Student>()
+        {
+            new Student(){roll=20,name= "Hari Bahadur"},
+            new Student(){roll=21,name= "Hari "},
+            new Student(){roll=22,name= " Bahadur"},
+            new Student(){roll=22,name= "Hary"}
+        };
+        //linkqueue
+        var StudNames =    
+            from s in students
+               where s.name == "Hary"
+            select s;
+            foreach (var student in StudNames)
+        {
+            Console.WriteLine(student.roll + " " + student.name);
+        }
+
+    }
+}
