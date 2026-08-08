0f794d8536c7f4e61f0567561f6cc7f8fe6b8b42
budhathokidai <budhathokirabi07@gmail.com>
2026-08-08 20:41:48 +0545

    Collection_Queue

---
+++ X_Collection/A_Gneric_Collection/B_queue/e_queue_peek/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace e_queue_peek;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Queue<string> names = new Queue<string>();
+        names.Enqueue("Ram");
+        names.Enqueue("Hari");
+        names.Enqueue("Shyam");
+        Console.WriteLine("The first element in the queue: " + names.Peek()); //Looks at the first element without removing it.
+        
+        foreach (var item in names)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
