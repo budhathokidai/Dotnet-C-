6abb8d35ad0ec0fa4cedfb6f199a05a5bfbd9868
budhathokidai <budhathokirabi07@gmail.com>
2026-08-08 20:44:55 +0545

    Collection_Stack

---
+++ X_Collection/A_Gneric_Collection/C_Stack/g_stack_to_array/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace g_stack_to_array;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        Stack<string> names=new Stack<string>();
+        names.Push("Hello");
+        names.Push("Hi");
+        names.Push("Ram");
+        names.Push("Hari");
+
+        string[] arr = names.ToArray();
+
+        foreach (var item in names)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
+
+/*Hari
+Ram
+Hi
+Hello*/
