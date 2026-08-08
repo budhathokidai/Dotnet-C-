e90c958c2cedbb6cbc6ec6fa6c0902a507b3d5ba
budhathokidai <budhathokirabi07@gmail.com>
2026-08-08 20:40:27 +0545

    Collection_Queue

---
+++ X_Collection/A_Gneric_Collection/B_queue/a_queue_t/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace a_queue_t;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        /* Queue<int> nums= new Queue<int>();
+        nums.Enqueue(23);
+        nums.Enqueue(3);
+        nums.Enqueue(2333);
+        nums.Enqueue(287);
+        nums.Dequeue();
+        foreach(var item in nums)
+        {
+            Console.WriteLine(item);
+        } */
+
+        //contain
+        /* Console.WriteLine(nums.Contains(3));
+        Console.WriteLine(nums.Contains(45));
+ */
+
+        //queue_t_string
+        Queue<string> names= new Queue<string>();
+        {
+            names.Enqueue("Ram");
+            names.Enqueue("shyam");
+            names.Enqueue("Hari");
+            names.Enqueue("Radha");
+            names.Enqueue("Hello");
+            names.Enqueue("Hi");
+            names.Enqueue("Bye bye");
+            Console.WriteLine("The number of elements in the queue"+names.Count);
+            Console.WriteLine("The first element in the queue = "+names.Peek());
+            foreach(var item in names)
+            {
+                Console.WriteLine(item);
+            }
+            
+        }
+        
+    }
+}
