17b924420dbf3658ba0e8152266c5a15d218721c
budhathokidai <budhathokirabi07@gmail.com>
2026-08-08 20:40:59 +0545

    Collection_Queue

---
+++ X_Collection/A_Gneric_Collection/B_queue/b_enqueue/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace b_enqueue;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Queue<int> nums=new Queue<int>();
+        nums.Enqueue(23);
+        nums.Enqueue(22);
+        nums.Enqueue(26);
+        nums.Enqueue(76);
+
+        foreach (var item in nums)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
