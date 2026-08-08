8a4451eabbca326b8d79ba1fe08a8721efa0ef95
budhathokidai <budhathokirabi07@gmail.com>
2026-08-08 20:44:02 +0545

    Collection_Stack

---
+++ X_Collection/A_Gneric_Collection/C_Stack/d_stack_count/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace d_stack_count;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Stack<int> nums = new Stack<int>();
+        nums.Push(45);
+        nums.Push(65);
+        nums.Push(5);
+        nums.Push(4);
+
+        Console.WriteLine("Number of elements in stack: " + nums.Count);
+        foreach (var item in nums)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
+
+/*Number of elements in stack: 4
+4
+5
+65
+45*/
