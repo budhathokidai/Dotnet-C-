0bcc970fab850ff34dacc06e16d9216fd0e5c117
budhathokidai <budhathokirabi07@gmail.com>
2026-08-08 20:42:03 +0545

    Collection_Queue

---
+++ X_Collection/A_Gneric_Collection/B_queue/f_queue_contain/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+
+namespace f_queue_contain;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Queue<int> nums= new Queue<int>();
+        nums.Enqueue(43);
+        nums.Enqueue(3);
+        nums.Enqueue(54);
+        nums.Enqueue(4663);
+
+        Console.WriteLine(nums.Contains(5));   //queue ma not contain huda false dekhauxa natra true
+        Console.WriteLine(nums.Contains(54));
+    }
+}
