793582054a788c731583b292d894ec747c5f9f00
budhathokidai <budhathokirabi07@gmail.com>
2026-08-08 20:43:42 +0545

    Collection_Stack

---
+++ X_Collection/A_Gneric_Collection/C_Stack/c_stack_peek/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace c_stack_peek;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Stack<int> nums = new Stack<int>();
+        nums.Push(45);
+        nums.Push(65);
+        nums.Push(5);
+        nums.Push(4);
+
+        Console.WriteLine("Top element in stack: " + nums.Peek());
+        foreach (var item in nums)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
+
+
+/*
+Top element in stack: 4
+4
+5
+65
+45*/
