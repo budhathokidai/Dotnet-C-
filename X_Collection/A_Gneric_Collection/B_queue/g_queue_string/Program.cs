5df0114e22cc16315b7a90ff1f451c82edec4157
budhathokidai <budhathokirabi07@gmail.com>
2026-08-08 20:42:18 +0545

    Collection_Queue

---
+++ X_Collection/A_Gneric_Collection/B_queue/g_queue_string/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace g_queue_string;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Queue<string> names = new Queue<string>();
+
+        names.Enqueue("Ram");
+        names.Enqueue("Shyam");
+        names.Enqueue("Hari");
+        names.Enqueue("Radha");
+        names.Enqueue("Hello");
+        names.Enqueue("Hi");
+        names.Enqueue("Bye bye");
+
+        Console.WriteLine("The number of elements in the queue = " + names.Count);
+
+        Console.WriteLine("The first element in the queue = " + names.Peek());
+
+        foreach (var item in names)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
