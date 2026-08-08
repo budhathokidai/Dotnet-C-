93d8c6b4d2cfb7f4f06058cb9792fcdaf40dadb3
budhathokidai <budhathokirabi07@gmail.com>
2026-08-08 20:44:18 +0545

    Collection_Stack

---
+++ X_Collection/A_Gneric_Collection/C_Stack/e_stack_from_array/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace e_stack_from_array;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] MyArray = new int[] { 1, 4, 2, 33, 4, 5 };
+        Stack<int> array = new Stack<int>(MyArray);
+
+        Console.WriteLine("Total elements = " + array.Count);
+
+        foreach (var item in array)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
+
+
+/*Total elements = 6
+5
+4
+33
+2
+4
+1*/
