0238a73434deea6c76106452eacc520ea4779da9
budhathokidai <budhathokirabi07@gmail.com>
2026-08-08 20:43:25 +0545

    Collection_Stack

---
+++ X_Collection/A_Gneric_Collection/C_Stack/b_stack_pop/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace b_stack_pop;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Stack<int> nums=new Stack<int>();  //LIFO
+        nums.Push(34);
+        nums.Push(33);
+        nums.Push(3);
+        nums.Pop();
+        foreach(var item in nums)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
