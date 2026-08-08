39fd7f54802be4061e8d678825e011bf5bba5911
budhathokidai <budhathokirabi07@gmail.com>
2026-08-08 20:41:15 +0545

    Collection_Queue

---
+++ X_Collection/A_Gneric_Collection/B_queue/c_Dequeue/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace c_Dequeue;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Queue<int> nums = new Queue<int>();
+        nums.Enqueue(23);
+        nums.Enqueue(22);
+        nums.Enqueue(26);
+        nums.Enqueue(76);
+        //FIFO = First In, First Out
+        nums.Dequeue();
+
+        foreach (var item in nums)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
