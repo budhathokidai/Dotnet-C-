950d655c683bddc440ecb2a623257e2ae9c57e93
budhathokidai <budhathokirabi07@gmail.com>
2026-08-09 18:45:12 +0545

    Collection_Stack

---
+++ X_Collection/A_Gneric_Collection/C_Stack/i_stack_contains/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace i_stack_contains;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        Stack<string> names= new Stack<string>();
+        names.Push("Rabi");
+        names.Push("Ram");
+        names.Push("Ravi");
+
+        Console.WriteLine(names.Contains("Rabi"));
+        Console.WriteLine(names.Contains("Kushal"));
+
+    }
+}
