8d787c0767090bd201b9aedf4ef5b752f573c390
budhathokidai <budhathokirabi07@gmail.com>
2026-08-09 18:46:31 +0545

    Collection_HashSet

---
+++ X_Collection/A_Gneric_Collection/D_Hashset/b_HashSet_Add/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace b_HashSet_Add;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        HashSet<int> nums = new HashSet<int>();
+
+            nums.Add(10);
+            nums.Add(20);
+            nums.Add(30);
+            nums.Add(20);
+
+            foreach (var item in nums)
+            {
+                Console.WriteLine(item);
+            }
+    }
+}
+
+/*10
+20
+30*/
