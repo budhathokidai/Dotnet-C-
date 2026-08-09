a39522f865c09002e940f4f6723596933310eb07
budhathokidai <budhathokirabi07@gmail.com>
2026-08-09 18:45:50 +0545

    Collection_HashSet

---
+++ X_Collection/A_Gneric_Collection/D_Hashset/a_HashSet_Initialize/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace a_HashSet_Initialize;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        HashSet<int> nums = new HashSet<int> { 12, 23, 3, 2, 4, 3, 5, 4, 6, 5 };   //HashSet is a collection used to store unique values.
+
+        Console.WriteLine("Elements in HashSet:");
+
+        foreach (var item in nums)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
+
+
+/*Elements in HashSet:
+12
+23
+3
+2
+4
+5
+6*/
