8cd40a1fed3f13aa69b61097f1091e315446bed2
budhathokidai <budhathokirabi07@gmail.com>
2026-08-09 18:46:46 +0545

    Collection_HashSet

---
+++ X_Collection/A_Gneric_Collection/D_Hashset/c_HashSet_Remove/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace c_HashSet_Remove;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        HashSet<int> nums = new HashSet<int>{10, 20, 30, 40};
+
+            nums.Remove(30);
+
+            foreach (var item in nums)
+            {
+                Console.WriteLine(item);
+            }
+    }
+}
+/*10
+20
+40*/
