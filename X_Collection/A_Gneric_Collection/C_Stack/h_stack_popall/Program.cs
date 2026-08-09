7d03a7c36a4e6897c2b47c7ebada58f18080d97e
budhathokidai <budhathokirabi07@gmail.com>
2026-08-09 18:44:34 +0545

    Collection_Stack

---
+++ X_Collection/A_Gneric_Collection/C_Stack/h_stack_popall/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace h_stack_popall;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] arr = new int[] { 23, 43, 54, 65, 76 };
+
+        Stack<int> myNums = new Stack<int>(arr);
+
+        Console.WriteLine("Total elements in stack = " + myNums.Count);
+
+        while (myNums.Count > 0)
+        {
+            Console.WriteLine("Removed = " + myNums.Pop());
+        }
+
+        Console.WriteLine("Total elements in stack = " + myNums.Count);
+
+    }
+}
+
+
+/*Total elements in stack = 5
+Removed = 76
+Removed = 65
+Removed = 54
+Removed = 43
+Removed = 23
+Total elements in stack = 0*/
