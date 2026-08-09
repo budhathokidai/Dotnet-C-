a0f5dc929430f9c7d239ea83081e0f191b2d2838
budhathokidai <budhathokirabi07@gmail.com>
2026-08-09 18:47:37 +0545

    Collection_HashSet

---
+++ X_Collection/A_Gneric_Collection/D_Hashset/f_HashSet_Clear/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace f_HashSet_Clear;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        HashSet<int> nums=new HashSet<int>(){12,23,45,221};
+        Console.WriteLine("Total Elements in set before clearing: "+nums.Count);
+        
+        foreach (var item in nums)
+        {
+            Console.WriteLine(item);
+        }
+        nums.Clear();
+        Console.WriteLine("Total Elements after clear:"+nums.Count);
+    }
+}
+
+/*Total Elements in set before clearing: 4
+12
+23
+45
+221
+Total Elements after clear:0*/
