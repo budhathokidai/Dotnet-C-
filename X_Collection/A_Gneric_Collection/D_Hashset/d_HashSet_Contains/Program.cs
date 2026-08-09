ff37fdcdd8e9cb0a5f5cf728e3dfa55ef58d1df1
budhathokidai <budhathokirabi07@gmail.com>
2026-08-09 18:47:02 +0545

    Collection_HashSet

---
+++ X_Collection/A_Gneric_Collection/D_Hashset/d_HashSet_Contains/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace d_HashSet_Contains;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        HashSet<int> nums = new HashSet<int> { 10, 20, 30, 40 };
+
+
+        Console.WriteLine(nums.Contains(10));
+        Console.WriteLine(nums.Contains(50));
+
+    }
+}
+/*True
+False*/
