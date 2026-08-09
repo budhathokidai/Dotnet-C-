2e90ef6106a25cf8bd432cd33bfc975c8890b181
budhathokidai <budhathokirabi07@gmail.com>
2026-08-09 18:47:18 +0545

    Collection_HashSet

---
+++ X_Collection/A_Gneric_Collection/D_Hashset/e_HashSet_Count/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace e_HashSet_Count;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        HashSet<int> nums=new HashSet<int>(){12,23,45,221};
+        Console.WriteLine("Total Elements in set: "+nums.Count);
+        foreach (var item in nums)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
+
+/*Total Elements in set: 4
+12
+23
+45
+221*/
