6e31f5857c7003aaa13a37d4c5c6eeb58eff4868
budhathokidai <budhathokirabi07@gmail.com>
2026-08-10 20:18:38 +0545

    Collection_Dictionary

---
+++ X_Collection/A_Gneric_Collection/E_Dictionary/e_Dictionary_ContainsValue/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace e_Dictionary_ContainsValue;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dictionary<int, string> rollnames = new Dictionary<int, string>();
+
+            rollnames.Add(23, "Ram");
+            rollnames.Add(24, "Hari");
+            rollnames.Add(25, "Shyam");
+
+            Console.WriteLine(rollnames.ContainsValue("Hari"));
+            Console.WriteLine(rollnames.ContainsValue("Sita"));
+    }
+}
+
+/*True
+False*/
