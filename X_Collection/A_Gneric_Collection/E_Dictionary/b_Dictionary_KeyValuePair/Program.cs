a3a15ee2516a76b54a2088b8fe1a81b67aa04e2c
budhathokidai <budhathokirabi07@gmail.com>
2026-08-10 20:17:56 +0545

    Collection_Dictionary

---
+++ X_Collection/A_Gneric_Collection/E_Dictionary/b_Dictionary_KeyValuePair/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace b_Dictionary_KeyValuePair;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+         Dictionary<int, string> rollnames = new Dictionary<int, string>();
+
+            rollnames.Add(23, "Ram");
+            rollnames.Add(24, "Hari");
+            rollnames.Add(25, "Shyam");
+
+            foreach (KeyValuePair<int, string> kvp in rollnames)
+            {
+                Console.WriteLine("Key is {0} and the value is {1}",kvp.Key,kvp.Value);
+            }
+    }
+}
+
+/*Key is 23 and the value is Ram
+Key is 24 and the value is Hari
+Key is 25 and the value is Shyam*/
