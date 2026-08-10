bb1052662a045333ba73cd062fdf8b9db6de46d9
budhathokidai <budhathokirabi07@gmail.com>
2026-08-10 20:18:50 +0545

    Collection_Dictionary

---
+++ X_Collection/A_Gneric_Collection/E_Dictionary/f_Dictionary_Remove/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace f_Dictionary_Remove;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dictionary<int, string> rollnames = new Dictionary<int, string>();
+
+            rollnames.Add(23, "Ram");
+            rollnames.Add(24, "Hari");
+            rollnames.Add(25, "Shyam");
+
+           Console.WriteLine("Before Remove");
+            foreach (var item in rollnames)
+            {
+
+                Console.WriteLine(item.Key + " = " + item.Value);
+            }
+            rollnames.Remove(24);
+            Console.WriteLine("After Remove");
+            foreach (var item in rollnames)
+            {
+
+                Console.WriteLine(item.Key + " = " + item.Value);
+            }
+    }
+}
+
+
+/*Before Remove
+23 = Ram
+24 = Hari
+25 = Shyam
+After Remove
+23 = Ram
+25 = Shyam*/
