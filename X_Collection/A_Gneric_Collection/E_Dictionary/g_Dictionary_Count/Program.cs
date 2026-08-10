3e09fc4963f0c4c936c1a8854f0b89da4ae3d831
budhathokidai <budhathokirabi07@gmail.com>
2026-08-10 20:19:12 +0545

    Collection_Dictionary

---
+++ X_Collection/A_Gneric_Collection/E_Dictionary/g_Dictionary_Count/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace g_Dictionary_Count;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dictionary<int, string> rollnames = new Dictionary<int, string>();
+        rollnames.Add(23, "Ram");
+        rollnames.Add(24, "Hari");
+        rollnames.Add(25, "Shyam");
+
+        Console.WriteLine("Total elements = " + rollnames.Count);
+
+    }
+}
+
+/*Total elements = 3*/
