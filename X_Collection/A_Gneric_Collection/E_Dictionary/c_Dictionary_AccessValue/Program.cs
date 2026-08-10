f87dc146b230776ae6323c35d9eb63d7a5fe5e61
budhathokidai <budhathokirabi07@gmail.com>
2026-08-10 20:18:11 +0545

    Collection_Dictionary

---
+++ X_Collection/A_Gneric_Collection/E_Dictionary/c_Dictionary_AccessValue/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace c_Dictionary_AccessValue;
+//You can directly access a value using its key.
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dictionary<int, string> rollnames = new Dictionary<int, string>();
+
+            rollnames.Add(23, "Ram");
+            rollnames.Add(24, "Hari");
+            rollnames.Add(25, "Shyam");
+
+            Console.WriteLine(rollnames[23]);
+            Console.WriteLine(rollnames[25]);
+    }
+}
+
+/*Ram
+Shyam*/
