a9606b52a4f9efa4cefc03449c931377f240c7ba
budhathokidai <budhathokirabi07@gmail.com>
2026-08-10 20:19:29 +0545

    Collection_Dictionary

---
+++ X_Collection/A_Gneric_Collection/E_Dictionary/h_Dictionary_Clear/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace h_Dictionary_Clear;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dictionary<int, string> rollnames =new Dictionary<int, string>();
+
+            rollnames.Add(23, "Ram");
+            rollnames.Add(24, "Hari");
+            rollnames.Add(25, "Shyam");
+
+            Console.WriteLine("Before Clearance:");
+            Console.WriteLine("Total elements = " + rollnames.Count);
+
+            rollnames.Clear();
+            Console.WriteLine("After Clearance:");
+            Console.WriteLine("Total elements = " + rollnames.Count);
+
+    }
+}
+
+/*Before Clearance:
+Total elements = 3
+After Clearance:
+Total elements = 0*/
