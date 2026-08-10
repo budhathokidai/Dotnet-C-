e7e515d624c8181fc37364e202cf66b6874acf49
budhathokidai <budhathokirabi07@gmail.com>
2026-08-10 20:17:38 +0545

    Collection_Dictionary

---
+++ X_Collection/A_Gneric_Collection/E_Dictionary/a_Dictionary_Add/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace a_Dictionary_Add;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dictionary<int, string> rollnames = new Dictionary<int, string>();
+        rollnames.Add(23, "Ram");
+        rollnames.Add(24, "Hari");
+        rollnames.Add(25, "Shyam");
+
+        foreach (var item in rollnames)
+        {
+            Console.WriteLine(item.Key);
+            Console.WriteLine(item.Value);
+        }
+    }
+}
+/*23
+Ram
+24
+Hari
+25
+Shyam*/
