529748a62373a80fd26fcb4d11f352800a24132c
budhathokidai <budhathokirabi07@gmail.com>
2026-08-10 20:19:47 +0545

    Collection_Dictionary

---
+++ X_Collection/A_Gneric_Collection/E_Dictionary/i_Dictionary_String/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace i_Dictionary_String;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var subjects = new Dictionary<string, string>()
+            {
+                {"education", "Nepali, Maths, Population, ICT"},
+                {"science", "Biology, Chemistry, Physics"},
+                {"humanities", "Sociology, Social Work"}
+            };
+
+        Console.WriteLine(subjects["education"]);
+        Console.WriteLine(subjects["humanities"]);
+
+        if (subjects.ContainsKey("education"))
+        {
+            Console.WriteLine(subjects["education"]);
+        }
+    }
+}
+
+/*Nepali, Maths, Population, ICT
+Sociology, Social Work
+Nepali, Maths, Population, ICT*/
