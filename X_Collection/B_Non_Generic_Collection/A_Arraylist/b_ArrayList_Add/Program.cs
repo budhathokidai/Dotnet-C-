5b0db543e11bb249dd8cd1bfe1052cdd25621049
budhathokidai <budhathokirabi07@gmail.com>
2026-08-13 21:21:30 +0545

    Non_Generic

---
+++ X_Collection/B_Non_Generic_Collection/A_Arraylist/b_ArrayList_Add/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace b_ArrayList_Add;
+
+using System.Collections;
+class Program
+{
+    static void Main(string[] args)
+    {
+        ArrayList namelist = new ArrayList();
+
+        namelist.Add("Rabi");
+        namelist.Add("Hari");
+        namelist.Add("24");
+        namelist.Add("45.6");
+
+        foreach (var item in namelist)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
+
+/*Rabi
+Hari
+24
+45.6*/
