c1a907e588aa91e2a23274d884dbfeceecc5781b
budhathokidai <budhathokirabi07@gmail.com>
2026-08-13 21:21:02 +0545

    Non-Generic

---
+++ X_Collection/B_Non_Generic_Collection/A_Arraylist/a_ArrayList_Create/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace a_ArrayList_Create;
+using System.Collections;
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Creates an empty ArrayList
+            ArrayList namelist = new ArrayList();
+
+            Console.WriteLine("ArrayList created successfully.");
+    }
+}
+
+/*ArrayList created successfully.*/
