182a3d29f3c418882ec03eb1592a3c1b1bb5c9e2
budhathokidai <budhathokirabi07@gmail.com>
2026-08-13 21:22:08 +0545

    Non_Generic

---
+++ X_Collection/B_Non_Generic_Collection/A_Arraylist/e_ArrayList_Insert/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace e_ArrayList_Insert;
+
+using System.Collections;
+class Program
+{
+    static void Main(string[] args)
+    {
+        ArrayList namelist = new ArrayList();
+
+        namelist.Add("Rabi");
+        namelist.Add("Hari");
+        namelist.Add("Ram");
+        namelist.Add("Shyam");
+
+        Console.WriteLine("===== Original List =====");
+
+       for(int i = 0; i < namelist.Count; i++)
+        {
+            Console.WriteLine(namelist[i]);
+        }
+
+        // Insert() is used to add an element at a specific index
+        namelist.Insert(1, "John");
+
+        Console.WriteLine("===== Updated List =====");
+
+         for(int i = 0; i < namelist.Count; i++)
+        {
+            Console.WriteLine(namelist[i]);
+        }
+    }
+}
+
+/*===== Original List =====
+Rabi
+Hari
+Ram
+Shyam
+===== Updated List =====
+Rabi
+John
+Hari
+Ram
+Shyam*/
