684a83c57f9a1f98bcb29682bb9031a19b8b4a12
budhathokidai <budhathokirabi07@gmail.com>
2026-08-13 21:21:45 +0545

    Non_Generic

---
+++ X_Collection/B_Non_Generic_Collection/A_Arraylist/c_ArrayList_Count/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace c_ArrayList_Count;
+
+using System.Collections;
+class Program
+{
+    static void Main(string[] args)
+    {
+        ArrayList namelist = new ArrayList();
+
+
+        namelist.Add("Rabi");
+        namelist.Add("Hari");
+        namelist.Add("Ram");
+        namelist.Add("Shyam");
+
+        Console.WriteLine("Total elements = " + namelist.Count);
+    }
+}
+
+/*Total elements = 4*/
