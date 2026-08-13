2af5e8d2297148e0f8e9e25b22c7e605244f76bc
budhathokidai <budhathokirabi07@gmail.com>
2026-08-13 21:22:20 +0545

    Non_Generic

---
+++ X_Collection/B_Non_Generic_Collection/A_Arraylist/f_ArrayList_Remove/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace f_ArrayList_Remove;
+
+using System.Collections;
+class Program
+{
+    static void Main(string[] args)
+    {
+        ArrayList namelist = new ArrayList();
+
+        namelist.Add("Rabi");
+        namelist.Add("Hari");
+        namelist.Add("Ram");
+        namelist.Add("Shyam");
+
+        // Remove() is used to remove an element by its value
+        namelist.Remove("Ram");
+        foreach (var item in namelist)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
+/*Rabi
+Hari
+Shyam*/
