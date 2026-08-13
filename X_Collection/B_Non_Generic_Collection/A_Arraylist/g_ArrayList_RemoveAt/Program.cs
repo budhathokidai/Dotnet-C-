39b3426f2b70cf4ab6e14d410c5963f19cf93792
budhathokidai <budhathokirabi07@gmail.com>
2026-08-13 21:22:34 +0545

    Non_Generic

---
+++ X_Collection/B_Non_Generic_Collection/A_Arraylist/g_ArrayList_RemoveAt/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace g_ArrayList_RemoveAt;
+
+using System.Collections;
+class Program
+{
+    static void Main(string[] args)
+    {
+        ArrayList namelist = new ArrayList();
+        namelist.Add("Rabi");
+        namelist.Add("Hari");
+        namelist.Add("Ram");
+        namelist.Add("Shyam");
+
+        // RemoveAt() is used to remove an element using its index
+        namelist.RemoveAt(1);
+
+        foreach (var item in namelist)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
+
+/*Rabi
+Ram
+Shyam*/
