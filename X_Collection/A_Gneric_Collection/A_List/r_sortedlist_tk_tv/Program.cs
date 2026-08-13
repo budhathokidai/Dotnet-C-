65eb5c774a9237403a86aad25f19a21a0226007c
budhathokidai <budhathokirabi07@gmail.com>
2026-08-13 20:42:06 +0545

    Collection_List

---
+++ X_Collection/A_Gneric_Collection/A_List/r_sortedlist_tk_tv/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace r_sortedlist_tk_tv;
+#nullable disable
+class Program
+{
+    static void Main(string[] args)
+    {
+        SortedList<int, string> rollnames = new SortedList<int, string>();
+        rollnames.Add(1, "Ram");
+        rollnames.Add(4, "Ramu");
+        rollnames.Add(2, "Ramuu");
+        rollnames.Add(5, null);
+        rollnames.Add(3, "Ramuuuu");
+
+       /*  //following will throw exception
+        rollnames.Add("Shyam",23);//compile time exception
+        rollnames.Add(1,"Hary");  //run time exception
+        rollnames.Add(null,"Appy"); */
+
+        rollnames[5]="Hira"; //assign with a value if value is not thera
+        rollnames[3]="Hira bete"; //updatea a value
+        Console.WriteLine(rollnames[4]);  //only for value
+
+        foreach (var item in rollnames)
+        {
+            Console.WriteLine("Key is {0} and value is {1}", item.Key, item.Value);
+        }
+
+    }
+}
