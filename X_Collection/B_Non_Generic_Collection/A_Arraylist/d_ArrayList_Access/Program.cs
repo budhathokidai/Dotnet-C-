9178a0bb9897185f9c6a27f912742e4309df3015
budhathokidai <budhathokirabi07@gmail.com>
2026-08-13 21:21:57 +0545

    Non_Generic

---
+++ X_Collection/B_Non_Generic_Collection/A_Arraylist/d_ArrayList_Access/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace d_ArrayList_Access;
+using System.Collections;
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+            ArrayList namelist = new ArrayList();
+            namelist.Add("Rabi");
+            namelist.Add("Hari");
+            namelist.Add("Ram");
+            Console.WriteLine(namelist[0]);
+            Console.WriteLine(namelist[1]);
+            Console.WriteLine(namelist[2]);
+    }
+}
+
+/*Rabi
+Hari
+Ram*/
