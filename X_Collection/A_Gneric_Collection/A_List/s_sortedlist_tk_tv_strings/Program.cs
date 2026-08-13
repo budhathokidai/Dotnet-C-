66dc4689a9169f4474afcfa111ba5ebfbf0a8d91
budhathokidai <budhathokirabi07@gmail.com>
2026-08-13 20:43:40 +0545

    Collection_List

---
+++ X_Collection/A_Gneric_Collection/A_List/s_sortedlist_tk_tv_strings/Program.cs
@@ -0,0 +1,20 @@
+﻿//What is collection-initilizer in Generic Types?Write a program in c# with creating a SortedList of string keys and string values using collection-initializer syntax.
+namespace s_sortedlist_tk_tv_strings;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*creating a sorted list of string keys,string values using collection initalizer syntax*/
+        SortedList<string,string> cities=new SortedList<string, string>()
+        {
+            {"Nepal","KTM"},
+            {"India","Delhi"},
+            {"Japan","Tokyo"}
+        };
+        foreach(var item in cities)
+        {
+            Console.WriteLine("{0} is the capital city of {1}",item.Value,item.Key);
+        }
+    }
+}
