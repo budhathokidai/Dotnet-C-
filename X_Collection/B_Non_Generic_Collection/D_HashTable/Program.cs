a8c4e285eb9b68a887193999ce3f7f990bd27e32
budhathokidai <budhathokirabi07@gmail.com>
2026-08-14 20:20:41 +0545

    Non_Generic

---
+++ X_Collection/B_Non_Generic_Collection/D_HashTable/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace D_HashTable;
+using System.Collections;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Hashtable hash=new Hashtable();
+        hash.Add(1,"Ram");
+        hash.Add(2,10);
+        hash.Add(3,10.4);
+        hash.Add(4,true);
+        Console.WriteLine("Hashtable Elements are:");
+        foreach (DictionaryEntry item in hash)
+        {
+            Console.WriteLine(item.Key +" "+item.Value);
+        }   
+    }
+}
