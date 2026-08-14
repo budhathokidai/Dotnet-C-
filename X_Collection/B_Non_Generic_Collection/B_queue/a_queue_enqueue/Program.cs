69e410be857dff438204d5c132e1d62a197a0d63
budhathokidai <budhathokirabi07@gmail.com>
2026-08-14 20:19:56 +0545

    Non_Generic

---
+++ X_Collection/B_Non_Generic_Collection/B_queue/a_queue_enqueue/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace a_queue_enqueue;
+
+using System.Collections;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Queue queue = new Queue();
+        queue.Enqueue("Ram");
+        queue.Enqueue("10");
+        queue.Enqueue("12.34");
+        queue.Enqueue(true);
+        Console.WriteLine("Peek elemnt:"+queue.Peek());
+        Console.WriteLine("Queue contains:"+ queue.Contains("Ram"));
+        Console.WriteLine("Queue Elements are:");
+        foreach (var item in queue)
+        {
+            Console.WriteLine(item);
+        }
+
+
+        /* queue.Dequeue();
+        Console.WriteLine("After dequeue:");
+        foreach (var item in queue)
+        {
+            Console.WriteLine(item);
+        } */
+
+    }
+}
