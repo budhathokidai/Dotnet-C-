c979328ca79abdf9c7e9c55a67526f1057a37628
budhathokidai <budhathokirabi07@gmail.com>
2026-08-14 20:20:18 +0545

    Non_Generic

---
+++ X_Collection/B_Non_Generic_Collection/C_Stack/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace C_Stack;
+using System.Collections;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Stack stack=new Stack();
+        stack.Push("Kushal");
+        stack.Push("10");
+        stack.Push("2345.345");
+        foreach (var item in stack)
+        {
+            Console.WriteLine(item);
+        }
+
+        Console.WriteLine("Stack contains:"+stack.Contains("Ram"));
+        Console.WriteLine("Peek element:"+stack.Peek());
+        stack.Pop();
+        /* Console.WriteLine("Elements after pop:");
+        foreach (var item in stack)
+        {
+            Console.WriteLine(item);
+        } */   
+    }
+}
