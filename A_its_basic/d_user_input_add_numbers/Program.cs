b26198eeebf847c3a037b1b20f8edb14d683edc6
budhathokidai <budhathokirabi07@gmail.com>
2026-08-16 19:06:36 +0545

    Basic_Updated

--- A_its_basic/d_user_input_add_numbers/Program.cs
+++ A_its_basic/d_user_input_add_numbers/Program.cs
@@ -5,10 +5,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Enter a first number:");
-        int a=Convert.ToInt32(Console.ReadLine());
+        int a = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine("Enter a Second number:");
-        int b=Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("The addition of two numbers is:"+(a+b));
+        int b = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("The addition of two numbers is:" + (a + b));
     }
 }
 
