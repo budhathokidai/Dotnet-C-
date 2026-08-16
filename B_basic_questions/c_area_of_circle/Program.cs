cd08edc9d170e39e9bb4b9973c409d7700871f1d
budhathokidai <budhathokirabi07@gmail.com>
2026-08-16 19:07:09 +0545

    Questions_Updated

--- B_basic_questions/c_area_of_circle/Program.cs
+++ B_basic_questions/c_area_of_circle/Program.cs
@@ -7,21 +7,21 @@ class Program
     void input()
     {
         Console.WriteLine("Enter radius of circle");
-        r=Convert.ToDouble(Console.ReadLine());
+        r = Convert.ToDouble(Console.ReadLine());
     }
     void output()
     {
-        double area= 3.14 *r*r;
-        Console.WriteLine("Area of circle:" +area);
+        double area = 3.14 * r * r;
+        Console.WriteLine("Area of circle:" + area);
     }
     static void Main(string[] args)
     {
-        Program p1=new Program();
+        Program p1 = new Program();
         p1.input();
         p1.output();
     }
 }
- 
- /*Enter radius of circle
+
+/*Enter radius of circle
 7
 Area of circle:153.86*/
