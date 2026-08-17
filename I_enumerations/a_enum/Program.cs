4a44b7c55b3b510e91ae2631ec42b6e48f35880c
budhathokidai <budhathokirabi07@gmail.com>
2026-08-17 21:10:53 +0545

    Enumerations

--- I_enumerations/a_enum/Program.cs
+++ I_enumerations/a_enum/Program.cs
@@ -2,14 +2,14 @@
 //An enum (enumeration) is a user-defined data type that is used to store a fixed set of named constants.
 class Program
 {
-    enum Days{Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Sturday};
+    enum Days { Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Sturday };
     static void Main(string[] args)
     {
         //enum to int
-        int firstDay=(int)Days.Sunday;
+        int firstDay = (int)Days.Sunday;
         Console.WriteLine(firstDay);
         //int to enum
-        var wd=(Days)5;
+        var wd = (Days)5;
         Console.WriteLine(wd);
     }
 }
