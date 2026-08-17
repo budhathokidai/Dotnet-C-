e1d9487832b341fc047c75f0ee152ea27f215d6e
budhathokidai <budhathokirabi07@gmail.com>
2026-08-17 21:10:32 +0545

    Structure

--- H_structure/a_struct/Program.cs
+++ H_structure/a_struct/Program.cs
@@ -5,7 +5,7 @@ struct Student
     public int roll;
     public void display(int roll)
     {
-        Console.WriteLine("Roll number="+roll);
+        Console.WriteLine("Roll number=" + roll);
     }
 }
 class Program
@@ -13,7 +13,7 @@ class Program
     static void Main(string[] args)
     {
         Student s1;
-        s1.roll=45;
+        s1.roll = 45;
         Console.WriteLine(s1.roll);
     }
 }
