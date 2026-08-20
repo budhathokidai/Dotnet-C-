ab95aabd71d083da6565bcc2a550d8146c644306
budhathokidai <budhathokirabi07@gmail.com>
2026-08-20 20:06:49 +0545

    LINQ

---
+++ Y_LINQ/e_linq_enum_marks/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace e_linq_enum_marks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       int[] marks = { 12, 32, 83, 94, 21, 84, 32, 122, 22, 93, 4 };
+
+        IEnumerable<int> marksQuery =
+            from m in marks
+            where m >= 80
+            select m;
+
+        foreach (var m in marksQuery)
+        {
+            Console.WriteLine(m);
+        }
+    }
+}
+
+/*83
+94
+84
+122
+93*/
