4ac049000435536fc0e054416f481e1ff4522d27
budhathokidai <budhathokirabi07@gmail.com>
2026-08-20 20:06:30 +0545

    LINQ

---
+++ Y_LINQ/d_linq_odd_numbers/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace d_linq_odd_numbers;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+         int[] num = { 12, 32, 3, 4, 21, 34, 32, 122, 22, 3, 4 };
+
+        int oddNum = num.Distinct().Count(n => n % 2 == 1);
+
+        Console.WriteLine("Total number of odd numbers: " + oddNum);
+    }
+}
