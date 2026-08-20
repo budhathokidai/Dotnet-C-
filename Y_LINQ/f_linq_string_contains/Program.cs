d3731d5b8ebd29f79a793b0fe4b5514814c01b95
budhathokidai <budhathokirabi07@gmail.com>
2026-08-20 20:07:02 +0545

    LINQ

---
+++ Y_LINQ/f_linq_string_contains/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace f_linq_string_contains;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] names = { "Ram", "Hari", "Rabi", "Sita" };
+
+        var myLinqQuery =
+            from name in names
+            where name.Contains("r")
+            select name;
+
+        foreach (var item in myLinqQuery)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
+/*Hari*/
