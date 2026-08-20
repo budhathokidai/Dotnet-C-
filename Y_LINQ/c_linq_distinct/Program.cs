a68abb7b815607938485244a95a763914ad03d29
budhathokidai <budhathokirabi07@gmail.com>
2026-08-20 20:06:20 +0545

    LINQ

---
+++ Y_LINQ/c_linq_distinct/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace c_linq_distinct;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] num = { 12, 32, 3, 4, 21, 34, 32, 122, 22, 3, 4 };
+
+        int uniqueNum = num.Distinct().Count();
+
+        Console.WriteLine("Number of unique elements: " + uniqueNum);
+    }
+}
