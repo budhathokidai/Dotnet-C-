4f4ff74f4a88f54bc07a8157e6a1b58b053a6e59
budhathokidai <budhathokirabi07@gmail.com>
2026-08-21 20:24:38 +0545

    LINQ

---
+++ Y_LINQ/i_linq_vowels/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace i_linq_vowels;
+#nullable disable
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a sentence:");
+
+        var sentence = Console.ReadLine();
+
+        var vowels = new char[] { 'A', 'E', 'I', 'O', 'U' };
+
+        var query = from letter in sentence.ToUpper()
+                    where vowels.Contains(letter)
+                    select letter;
+
+        var count = query.Count();
+
+        Console.WriteLine("Number of vowels = " + count);
+    }
+}
+
+/*Enter a sentence:
+Hello my name is rabi budahathoki from gatthaghar bhaktapur
+Number of vowels = 19*/
