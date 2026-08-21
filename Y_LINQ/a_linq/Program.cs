4b9c9c7b2fcf1c20845fbc06cffd659e34e5e43f
budhathokidai <budhathokirabi07@gmail.com>
2026-08-21 20:24:03 +0545

    LINQ

--- Y_LINQ/a_linq/Program.cs
+++ Y_LINQ/a_linq/Program.cs
@@ -1,16 +1,16 @@
 ﻿namespace a_linq;
-
+#nullable disable
 class Program
 {
     static void Main(string[] args)
     {
-        string[] students = { "Rabi", "Kushal", "Ram", "Rama" };
+         string[] students = { "Rabi", "Kushal", "Ram", "Rama" };
         var nameSort = from name in students
                        orderby name
                        select name;  //linq
         foreach (var name in nameSort)
         {
             Console.WriteLine(name);
-        }
+        } 
     }
 }
